fed05969480f0578bea0f45d172e4c91b55bb3a9
EvenSol <esolbraa@gmail.com>
2021-05-26 14:08:03 +0200

    example of adding a new EOS

---
+++ src/test/java/neqsim/thermo/system/SystemUMRPRUMCEosNewTest.java
@@ -0,0 +1,12 @@
+package neqsim.thermo.system;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SystemUMRPRUMCEosNewTest {
+
+	static neqsim.thermo.system.SystemInterface testSystem = null;
+
+}
