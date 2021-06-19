f9ab29b7256736bc9a42dabe6a61b3fafddc22d7
Åsmund Våge Fannemel <ASMF@equinor.com>
2021-06-19 12:09:19 +0200

    style: formatting

--- src/main/java/neqsim/thermo/mixingRule/CPAMixingInterface.java
+++ src/main/java/neqsim/thermo/mixingRule/CPAMixingInterface.java
@@ -12,31 +12,31 @@ import neqsim.thermo.phase.PhaseInterface;
  * @version
  */
 public interface CPAMixingInterface extends java.io.Serializable {
-        // public double calcXi(int siteNumber, int compnumb, PhaseInterface phase,
-        // double temperature, double pressure, int numbcomp);
+    // public double calcXi(int siteNumber, int compnumb, PhaseInterface phase,
+    // double temperature, double pressure, int numbcomp);
 
-        public double calcDeltadT(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
-                        double temperature, double pressure, int numbcomp);
+    public double calcDeltadT(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
-        public double calcDeltadTdV(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2,
-                        PhaseInterface phase, double temperature, double pressure, int numbcomp);
+    public double calcDeltadTdV(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
-        public double calcDeltadTdT(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2,
-                        PhaseInterface phase, double temperature, double pressure, int numbcomp);
+    public double calcDeltadTdT(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
-        public double calcXi(int[][][] assosScheme, int[][][][] assosScheme2, int siteNumber, int compnumb,
-                        PhaseInterface phase, double temperature, double pressure, int numbcomp);
+    public double calcXi(int[][][] assosScheme, int[][][][] assosScheme2, int siteNumber, int compnumb,
+            PhaseInterface phase, double temperature, double pressure, int numbcomp);
 
-        public double calcDelta(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
-                        double temperature, double pressure, int numbcomp);
+    public double calcDelta(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
-        public double calcDeltadN(int derivativeComp, int siteNumber1, int siteNumber2, int compnumb1, int compnumb2,
-                        PhaseInterface phase, double temperature, double pressure, int numbcomp);
+    public double calcDeltadN(int derivativeComp, int siteNumber1, int siteNumber2, int compnumb1, int compnumb2,
+            PhaseInterface phase, double temperature, double pressure, int numbcomp);
 
-        public double calcDeltadV(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
-                        double temperature, double pressure, int numbcomp);
+    public double calcDeltadV(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
-        public double calcDeltaNog(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
-                        double temperature, double pressure, int numbcomp);
+    public double calcDeltaNog(int siteNumber1, int siteNumber2, int compnumb1, int compnumb2, PhaseInterface phase,
+            double temperature, double pressure, int numbcomp);
 
 }
