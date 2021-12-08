e290343da964e5ec02c55362d608082b823fef43
EvenSol <esolbraa@gmail.com>
2021-12-08 15:02:49 +0100

    Create EclipseFluidReader.java

---
+++ src/main/java/neqsim/thermo/util/reader/EclipseFluidReader.java
@@ -0,0 +1,16 @@
+package neqsim.thermo.util.reader;
+
+import neqsim.thermo.system.SystemInterface;
+
+public class EclipseFluidReader {
+	
+	public static SystemInterface read(String input) {
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
