3374d4890144f36e12be0079f61839c03b87893c
Åsmund Våge Fannemel <fannemel@gmail.com>
2021-12-15 22:34:28 +0100

    more

--- src/main/java/neqsim/processSimulation/processEquipment/util/Calculator.java
+++ src/main/java/neqsim/processSimulation/processEquipment/util/Calculator.java
@@ -1,22 +1,17 @@
 package neqsim.processSimulation.processEquipment.util;
 
 import java.util.ArrayList;
-
 import neqsim.processSimulation.processEquipment.ProcessEquipmentBaseClass;
 import neqsim.processSimulation.processEquipment.ProcessEquipmentInterface;
-import neqsim.processSimulation.processEquipment.stream.Stream;;
+import neqsim.processSimulation.processEquipment.stream.Stream;
 
 public class Calculator extends ProcessEquipmentBaseClass {
-
     ArrayList<ProcessEquipmentInterface> inputVariable = new ArrayList<ProcessEquipmentInterface>();
     private ProcessEquipmentInterface outputVariable;
     String type = "sumTEG";
 
     public Calculator(String name) {
         super(name);
-    }
-
-    public static void main(String[] args) {
     }
 
     public void addInputVariable(ProcessEquipmentInterface unit) {
@@ -33,15 +28,17 @@ public class Calculator extends ProcessEquipmentBaseClass {
 
         if (name.equals("MEG makeup calculator")) {
             for (int i = 0; i < inputVariable.size(); i++) {
-                sum += inputVariable.get(i).getFluid().getPhase(0).getComponent("MEG").getFlowRate("kg/hr");
+                sum += inputVariable.get(i).getFluid().getPhase(0).getComponent("MEG")
+                        .getFlowRate("kg/hr");
             }
         } else {
             for (int i = 0; i < inputVariable.size(); i++) {
-                sum += inputVariable.get(i).getFluid().getPhase(0).getComponent("TEG").getFlowRate("kg/hr");
+                sum += inputVariable.get(i).getFluid().getPhase(0).getComponent("TEG")
+                        .getFlowRate("kg/hr");
             }
         }
 
-        //System.out.println("make up MEG " + sum);
+        // System.out.println("make up MEG " + sum);
         outputVariable.getFluid().setTotalFlowRate(sum, "kg/hr");
         try {
             ((Stream) outputVariable).setFlowRate(sum, "kg/hr");
