51a4ca7d1278a50880b5d66518b2e844579cfffa
EvenSol <esolbraa@gmail.com>
2022-01-04 16:51:09 +0100

    Update SystemPrEosTest.java

--- src/test/java/neqsim/thermo/system/SystemPrEosTest.java
+++ src/test/java/neqsim/thermo/system/SystemPrEosTest.java
@@ -1,62 +1,88 @@
 package neqsim.thermo.system;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import neqsim.thermodynamicOperations.ThermodynamicOperations;
 
+class SystemPrEoSTest {
+    static neqsim.thermo.system.SystemInterface testSystem = null;
+    static neqsim.thermo.ThermodynamicModelTest testModel = null;
 
-public class SystemPrEosTest {
-    public static ThermodynamicOperations testOps;
-
-    /**
-     * <p>
-     * setUp.
-     * </p>
-     */
     @BeforeAll
     public static void setUp() {
-        SystemInterface testSystem = new SystemPrEos(260.0, 5.0);
-
-        testOps = new ThermodynamicOperations(testSystem);
-        // testSystem.addComponent("nitrogen", 14.0);
-        // testSystem.addComponent("methane", 93.505);
-        testSystem.addComponent("methane", 0.3371);
-        testSystem.addComponent("ethane", 0.3745);
-        testSystem.addComponent("propane", 0.1);
-        testSystem.addComponent("n-butane", 0.7153);
-        // testSystem2.addComponent("propane", 1.008);
-        // testSystem.addComponent("i-butane", 1.050);
-        testSystem.addComponent("n-butane", 10.465);
-        /*
-         * testSystem.addComponent("n-pentane", 2653);
-         *
-         * testSystem.addComponent("n-hexane", 514.2); testSystem.addComponent("benzene", 61.03);
-         * testSystem.addComponent("toluene", 24.63); testSystem.addComponent("c-hexane", 45.23);
-         * testSystem.addComponent("n-heptane", 93.83); testSystem.addComponent("n-octane", 12.17);
-         * testSystem.addComponent("n-nonane", 0.03); testSystem.addComponent("nC10", 0.01);
-         */
-        // testSystem.addComponent("CO2", 1.0);
-        // testSystem.addComponent("water", 200.0e-4);
-        // testSystem.addComponent("i-butane", 0.6013);
-        testSystem.addComponent("n-butane", 1.018);
-        // testSystem.addComponent("n-hexane", 0.1018);
-        // testSystem.addComponent("n-heptane", 0.1757);
-
-        testSystem.createDatabase(true);
-        // 1- orginal no interaction 2- classic w interaction
-        // 3- Huron-Vidal 4- Wong-Sandler
-        testSystem.setMixingRule(2);
-        // testSystem.setMixingRule("HV", "UNIFAC_PSRK");
-        testSystem.init(0);
-    }
-
-    @Test
-    public void TestcalcPTphaseEnvelope() {
-        testOps.calcPTphaseEnvelope(true);
-    }
-
-    @Test
-    public void testTPflash() {
+        testSystem = new neqsim.thermo.system.SystemPrEos(298.0, 10.0);
+        testSystem.addComponent("nitrogen", 0.01);
+        testSystem.addComponent("CO2", 0.01);
+        testSystem.addComponent("methane", 0.68);
+        testSystem.addComponent("ethane", 0.1);
+        testSystem.addComponent("n-heptane", 0.2);
+        testSystem.setMixingRule("classic");
+        testModel = new neqsim.thermo.ThermodynamicModelTest(testSystem);
+        ThermodynamicOperations testOps = new ThermodynamicOperations(testSystem);
         testOps.TPflash();
+        testSystem.initProperties();
+    }
+
+    @Test
+    @DisplayName("test a TPflash2")
+    public void testTPflash2() {
+        assertEquals(testSystem.getNumberOfPhases(), 2);
+    }
+
+    @Test
+    @DisplayName("test a TPflash of the fluid (should return two phases)")
+    public void testTPflash() {
+        assertEquals(testSystem.getNumberOfPhases(), 2);
+    }
+
+    @Test
+    @DisplayName("test the fugacity coeficients calcutated")
+    public void testFugacityCoefficients() {
+        assertTrue(testModel.checkFugasityCoeffisients());
+    }
+
+    @Test
+    @DisplayName("test derivative of fugacity coeficients with respect to pressure")
+    public void checkFugasityCoeffisientsDP() {
+        assertTrue(testModel.checkFugasityCoeffisientsDP());
+    }
+
+    @Test
+    @DisplayName("test derivative of fugacity coeficients with respect to temperature")
+    public void checkFugasityCoeffisientsDT() {
+        assertTrue(testModel.checkFugasityCoeffisientsDT());
+    }
+
+    @Test
+    @DisplayName("test derivative of fugacity coeficients with respect to composition")
+    public void checkFugasityCoeffisientsDn() {
+        assertTrue(testModel.checkFugasityCoeffisientsDn());
+    }
+
+    @Test
+    @DisplayName("test derivative of fugacity coeficients with respect to composition (2nd method)")
+    public void checkFugasityCoeffisientsDn2() {
+        assertTrue(testModel.checkFugasityCoeffisientsDn2());
+    }
+
+    @Test
+    @DisplayName("calculate compressibility of gas phase")
+    public void checkCompressibility() {
+        System.out.println("gas compressibility " + testSystem.getPhase("gas").getZ());
+        assertEquals(testSystem.getPhase("gas").getZ(), 0.9708455641951108, 1e-5);
+    }
+    
+    @Test
+    @DisplayName("calculate properties when flow rate is 0")
+    public void calcProperties() {
+    	testSystem.setTotalFlowRate(1.0, "mol/sec");
+    	 ThermodynamicOperations testOps = new ThermodynamicOperations(testSystem);
+    	 testOps.TPflash();
+         testSystem.initProperties();
+         System.out.print("enthalpy " + testSystem.getEnthalpy("kJ/kg"));
+    	assertEquals(testSystem.getEnthalpy("kJ/kg"), -165.60627184389855, Math.abs(-165.60627184389855/1000.0));
     }
 }
