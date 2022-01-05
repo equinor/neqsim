327a33e095caeb0a31a53a61e0b03f1a09e68208
Åsmund Våge Fannemel <fannemel@gmail.com>
2022-01-05 12:22:34 +0100

    revert

--- src/test/java/neqsim/processSimulation/util/example/MasstransferMeOH.java
+++ src/test/java/neqsim/processSimulation/util/example/MasstransferMeOH.java
@@ -25,50 +25,43 @@ public class MasstransferMeOH {
         neqsim.thermo.system.SystemInterface feedGas =
                 new neqsim.thermo.system.SystemSrkCPAstatoil(273.15 + 42.0, 10.00);
 
-                StreamSaturatorUtil saturatedFeedGas = new StreamSaturatorUtil(dryFeedGas);
-                saturatedFeedGas.setName("water saturator");
+        feedGas.addComponent("methane", 83.88);
+        feedGas.addComponent("water", 0.0);
+        feedGas.addComponent("methanol", 0);
+        feedGas.createDatabase(true);
+        feedGas.setMixingRule(10);
+        feedGas.setMultiPhaseCheck(true);
 
-                Stream waterSaturatedFeedGas = new Stream(saturatedFeedGas.getOutStream());
-                waterSaturatedFeedGas.setName("water saturated feed gas");
+        Stream dryFeedGas = new Stream("dry feed gas", feedGas);
+        dryFeedGas.setFlowRate(1.23, "MSm3/day");
+        dryFeedGas.setTemperature(10.4, "C");
+        dryFeedGas.setPressure(52.21, "bara");
 
-                neqsim.thermo.system.SystemInterface feedMeOH =
-                                (neqsim.thermo.system.SystemInterface) feedGas.clone();
-                feedMeOH.setMolarComposition(new double[] {0.0, 0.0, 1.0});
+        StreamSaturatorUtil saturatedFeedGas = new StreamSaturatorUtil(dryFeedGas);
+        saturatedFeedGas.setName("water saturator");
 
-                Stream MeOHFeed = new Stream("lean TEG to absorber", feedMeOH);
-                MeOHFeed.setFlowRate(680.5, "kg/hr");
-                MeOHFeed.setTemperature(10.4, "C");
-                MeOHFeed.setPressure(52.21, "bara");
+        Stream waterSaturatedFeedGas = new Stream(saturatedFeedGas.getOutStream());
+        waterSaturatedFeedGas.setName("water saturated feed gas");
 
         neqsim.thermo.system.SystemInterface feedMeOH =
                 (neqsim.thermo.system.SystemInterface) feedGas.clone();
         feedMeOH.setMolarComposition(new double[] {0.0, 0.0, 1.0});
 
-                neqsim.processSimulation.processEquipment.util.NeqSimUnit pipeline =
-                                new neqsim.processSimulation.processEquipment.util.NeqSimUnit(
-                                                mainMixer.getOutStream(), "pipeline", "stratified");
-                pipeline.setLength(123.01);
+        Stream MeOHFeed = new Stream("lean TEG to absorber", feedMeOH);
+        MeOHFeed.setFlowRate(680.5, "kg/hr");
+        MeOHFeed.setTemperature(10.4, "C");
+        MeOHFeed.setPressure(52.21, "bara");
 
-                GasScrubber scrubber = new GasScrubber("gas scrub", pipeline.getOutStream());
+        StaticMixer mainMixer = new StaticPhaseMixer("gas MeOH mixer");
+        mainMixer.addStream(waterSaturatedFeedGas);
+        mainMixer.addStream(MeOHFeed);
 
         neqsim.processSimulation.processEquipment.util.NeqSimUnit pipeline =
                 new neqsim.processSimulation.processEquipment.util.NeqSimUnit(
                         mainMixer.getOutStream(), "pipeline", "stratified");
         pipeline.setLength(123.01);
 
-                neqsim.processSimulation.processSystem.ProcessSystem operations =
-                                new neqsim.processSimulation.processSystem.ProcessSystem();
-                operations.add(dryFeedGas);
-                operations.add(saturatedFeedGas);
-                operations.add(waterSaturatedFeedGas);
-                operations.add(MeOHFeed);
-                operations.add(mainMixer);
-                operations.add(pipeline);
-                operations.add(pipeline);
-                operations.add(scrubber);
-                operations.add(gasFromScrubber);
-                operations.run();
-                // operations.run();
+        GasScrubber scrubber = new GasScrubber("gas scrub", pipeline.getOutStream());
 
         Stream gasFromScrubber = new Stream(scrubber.getGasOutStream());
 
