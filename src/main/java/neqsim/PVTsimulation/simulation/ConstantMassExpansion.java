684af5665b48e5f9183e5c7fb51bc860c28480fb
fannemel <fannemel@gmail.com>
2022-01-06 14:50:13 +0100

    Update ConstantMassExpansion.java

--- src/main/java/neqsim/PVTsimulation/simulation/ConstantMassExpansion.java
+++ src/main/java/neqsim/PVTsimulation/simulation/ConstantMassExpansion.java
@@ -191,8 +191,7 @@ public class ConstantMassExpansion extends BasePVTsimulation {
                                 / 1000.0};
                 function.setInitialGuess(guess);
 
-                SystemInterface tempSystem = getThermoSystem();//
-                                                               // getThermoSystem().clone();
+                SystemInterface tempSystem = getThermoSystem(); // getThermoSystem().clone();
 
                 tempSystem.setTemperature(temperature);
                 tempSystem.setPressure(pressures[i]);
