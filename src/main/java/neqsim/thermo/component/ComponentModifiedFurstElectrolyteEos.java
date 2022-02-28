06c371bd2eddf94456c7c1d9df4881e5c1be46cf
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-02-28 18:06:55 +0100

    missed

--- src/main/java/neqsim/thermo/component/ComponentModifiedFurstElectrolyteEos.java
+++ src/main/java/neqsim/thermo/component/ComponentModifiedFurstElectrolyteEos.java
@@ -34,7 +34,6 @@ public class ComponentModifiedFurstElectrolyteEos extends ComponentSrk {
      */
     public ComponentModifiedFurstElectrolyteEos(double moles) {
         super(moles);
-        numberOfMoles = moles;
     }
 
     /**
