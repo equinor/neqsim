df8c600ca1cd0692b1e30fe897452cb169a01bde
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-03-03 12:10:39 +0100

    bugfix: SystemProperties index error and pressure unit feat: new function getnames to get names of system properties docfix

--- src/test/java/neqsim/thermo/system/SystemPropertiesTest.java
+++ src/test/java/neqsim/thermo/system/SystemPropertiesTest.java
@@ -1,10 +1,12 @@
 package neqsim.thermo.system;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SystemPropertiesTest {
     @Test
     void testGetProperties() {
-
+        String[] name = SystemProperties.getPropertyNames();
+        Assertions.assertEquals(70, name.length);
     }
 }
