5c4d2d9d4bbcaafbbd4b809490f09e88cf9282ec
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-03-16 22:54:23 +0100

    chore: added missing serialVersionUID for serializable classes

--- src/main/java/neqsim/fluidMechanics/geometryDefinitions/internalGeometry/packings/Packing.java
+++ src/main/java/neqsim/fluidMechanics/geometryDefinitions/internalGeometry/packings/Packing.java
@@ -16,106 +16,105 @@ import neqsim.util.NamedBaseClass;
  * @version $Id: $Id
  */
 public class Packing extends NamedBaseClass implements PackingInterface {
-    double voidFractionPacking = 0.951, size = 0, surfaceAreaPrVolume = 112.6;
+  private static final long serialVersionUID = 1L;
+  double voidFractionPacking = 0.951, size = 0, surfaceAreaPrVolume = 112.6;
 
-    /**
-     * <p>
-     * Constructor for Packing.
-     * </p>
-     */
-    @Deprecated
-    public Packing() {
-        super("Packing");
+  /**
+   * <p>
+   * Constructor for Packing.
+   * </p>
+   */
+  @Deprecated
+  public Packing() {
+    super("Packing");
+  }
+
+  /**
+   * <p>
+   * Constructor for Packing.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   */
+  public Packing(String name) {
+    super(name);
+    try {
+      System.out.println("init packing");
+      neqsim.util.database.NeqSimDataBase database = new neqsim.util.database.NeqSimDataBase();
+      java.sql.ResultSet dataSet =
+          database.getResultSet(("SELECT * FROM packing WHERE name='" + name + "'"));
+      dataSet.next();
+      size = 1e-3 * Double.parseDouble(dataSet.getString("size")); // C
+      surfaceAreaPrVolume = Double.parseDouble(dataSet.getString("surfaceAreaPrVolume"));
+      voidFractionPacking = Double.parseDouble(dataSet.getString("voidFraction"));
+      System.out.println("packing ok");
     }
 
-    /**
-     * <p>
-     * Constructor for Packing.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     */
-    public Packing(String name) {
-        super(name);
-        try {
-            System.out.println("init packing");
-            neqsim.util.database.NeqSimDataBase database =
-                    new neqsim.util.database.NeqSimDataBase();
-            java.sql.ResultSet dataSet =
-                    database.getResultSet(("SELECT * FROM packing WHERE name='" + name + "'"));
-            dataSet.next();
-            size = 1e-3 * Double.parseDouble(dataSet.getString("size")); // C
-            surfaceAreaPrVolume = Double.parseDouble(dataSet.getString("surfaceAreaPrVolume"));
-            voidFractionPacking = Double.parseDouble(dataSet.getString("voidFraction"));
-            System.out.println("packing ok");
-        }
+    catch (Exception e) {
+      String err = e.toString();
+      System.out.println(err);
+    }
+  }
 
-        catch (Exception e) {
-            String err = e.toString();
-            System.out.println(err);
-        }
+  /**
+   * <p>
+   * Constructor for Packing.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param material a {@link java.lang.String} object
+   * @param size a int
+   */
+  public Packing(String name, String material, int size) {
+    super(name);
+    try {
+      System.out.println("init packing");
+      neqsim.util.database.NeqSimDataBase database = new neqsim.util.database.NeqSimDataBase();
+      java.sql.ResultSet dataSet = database.getResultSet(("SELECT * FROM packing WHERE name='"
+          + name + "' AND size=" + size + " AND material='" + material + "'"));
+      dataSet.next();
+      this.size = 1e-3 * Double.parseDouble(dataSet.getString("size")); // C
+      surfaceAreaPrVolume = Double.parseDouble(dataSet.getString("surfaceAreaPrVolume"));
+      voidFractionPacking = Double.parseDouble(dataSet.getString("voidFraction"));
+      System.out.println("packing ok");
     }
 
-    /**
-     * <p>
-     * Constructor for Packing.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @param material a {@link java.lang.String} object
-     * @param size a int
-     */
-    public Packing(String name, String material, int size) {
-        super(name);
-        try {
-            System.out.println("init packing");
-            neqsim.util.database.NeqSimDataBase database =
-                    new neqsim.util.database.NeqSimDataBase();
-            java.sql.ResultSet dataSet = database.getResultSet(("SELECT * FROM packing WHERE name='"
-                    + name + "' AND size=" + size + " AND material='" + material + "'"));
-            dataSet.next();
-            this.size = 1e-3 * Double.parseDouble(dataSet.getString("size")); // C
-            surfaceAreaPrVolume = Double.parseDouble(dataSet.getString("surfaceAreaPrVolume"));
-            voidFractionPacking = Double.parseDouble(dataSet.getString("voidFraction"));
-            System.out.println("packing ok");
-        }
-
-        catch (Exception e) {
-            String err = e.toString();
-            System.out.println(err);
-        }
+    catch (Exception e) {
+      String err = e.toString();
+      System.out.println(err);
     }
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getSurfaceAreaPrVolume() {
-        return surfaceAreaPrVolume;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getSurfaceAreaPrVolume() {
+    return surfaceAreaPrVolume;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getVoidFractionPacking() {
-        return voidFractionPacking;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getVoidFractionPacking() {
+    return voidFractionPacking;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void setVoidFractionPacking(double voidFractionPacking) {
-        this.voidFractionPacking = voidFractionPacking;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void setVoidFractionPacking(double voidFractionPacking) {
+    this.voidFractionPacking = voidFractionPacking;
+  }
 
-    /**
-     * Setter for property size.
-     *
-     * @param size New value of property size.
-     */
-    public void setSize(double size) {
-        this.size = size;
-    }
+  /**
+   * Setter for property size.
+   *
+   * @param size New value of property size.
+   */
+  public void setSize(double size) {
+    this.size = size;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getSize() {
-        return size;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getSize() {
+    return size;
+  }
 }
--- src/main/java/neqsim/standards/gasQuality/SulfurSpecificationMethod.java
+++ src/main/java/neqsim/standards/gasQuality/SulfurSpecificationMethod.java
@@ -11,55 +11,56 @@ import neqsim.thermo.system.SystemInterface;
  * @version $Id: $Id
  */
 public class SulfurSpecificationMethod extends neqsim.standards.Standard {
-    SystemInterface thermoSystem;
-    String unit = "ppm";
-    double H2Scontent = 0.0;
+  private static final long serialVersionUID = 1L;
+  SystemInterface thermoSystem;
+  String unit = "ppm";
+  double H2Scontent = 0.0;
 
-    /**
-     * <p>
-     * Constructor for SulfurSpecificationMethod.
-     * </p>
-     *
-     * @param thermoSystem a {@link neqsim.thermo.system.SystemInterface} object
-     */
-    public SulfurSpecificationMethod(SystemInterface thermoSystem) {
-        super("SulfurSpecificationMethod", "Sulfur Specification Method", thermoSystem);
-    }
+  /**
+   * <p>
+   * Constructor for SulfurSpecificationMethod.
+   * </p>
+   *
+   * @param thermoSystem a {@link neqsim.thermo.system.SystemInterface} object
+   */
+  public SulfurSpecificationMethod(SystemInterface thermoSystem) {
+    super("SulfurSpecificationMethod", "Sulfur Specification Method", thermoSystem);
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public void calculate() {
-        thermoSystem.init(0);
-    }
+  /** {@inheritDoc} */
+  @Override
+  public void calculate() {
+    thermoSystem.init(0);
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getValue(String returnParameter, java.lang.String returnUnit) {
-        thermoSystem.init(0);
-        if (thermoSystem.getPhase(0).hasComponent("H2S")) {
-            if (returnParameter.equals("H2S content")) {
-                H2Scontent = thermoSystem.getPhase(0).getComponent("H2S").getx() * 1e6;
-                return H2Scontent;
-            }
-        }
-        return 0.0;
+  /** {@inheritDoc} */
+  @Override
+  public double getValue(String returnParameter, java.lang.String returnUnit) {
+    thermoSystem.init(0);
+    if (thermoSystem.getPhase(0).hasComponent("H2S")) {
+      if (returnParameter.equals("H2S content")) {
+        H2Scontent = thermoSystem.getPhase(0).getComponent("H2S").getx() * 1e6;
+        return H2Scontent;
+      }
     }
+    return 0.0;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getValue(String returnParameter) {
-        return getValue(returnParameter, "");
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getValue(String returnParameter) {
+    return getValue(returnParameter, "");
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public String getUnit(String returnParameter) {
-        return unit;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public String getUnit(String returnParameter) {
+    return unit;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public boolean isOnSpec() {
-        return true;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public boolean isOnSpec() {
+    return true;
+  }
 }
