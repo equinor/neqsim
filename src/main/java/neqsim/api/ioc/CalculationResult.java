adac5487911cff7858a7cef4f0d14e7637943e83
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-03-26 14:19:54 +0100

    feat: added equality comparator for CalculationResult

--- src/main/java/neqsim/api/ioc/CalculationResult.java
+++ src/main/java/neqsim/api/ioc/CalculationResult.java
@@ -1,18 +1,41 @@
 package neqsim.api.ioc;
 
+import java.util.Arrays;
+
 /**
  *
  * @author jo.lyshoel
  */
 public class CalculationResult {
+  public Double[][] fluidProperties;
+  public String[] calculationError;
 
-    public Double[][] fluidProperties;
-    public String[] calculationError;
+  public CalculationResult() {}
 
-    public CalculationResult() {}
+  public CalculationResult(Double[][] fluidProperties, String[] calculationError) {
+    this.fluidProperties = fluidProperties;
+    this.calculationError = calculationError;
+  }
 
-    public CalculationResult(Double[][] fluidProperties, String[] calculationError) {
-        this.fluidProperties = fluidProperties;
-        this.calculationError = calculationError;
-    }
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + Arrays.hashCode(calculationError);
+    result = prime * result + Arrays.deepHashCode(fluidProperties);
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    CalculationResult other = (CalculationResult) obj;
+    return Arrays.equals(calculationError, other.calculationError)
+        && Arrays.deepEquals(fluidProperties, other.fluidProperties);
+  }
 }
