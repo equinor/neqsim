3fe37613224c3b51e72d91ef9607a4d9dfaa2162
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-05-18 22:50:29 +0200

    Flashtestupdate (#408)
    * pipe friction update
    
    * Create TPFlashTest.java
    
    * Update PipeFlowSystemTest.java

---
+++ src/test/java/neqsim/thermodynamicOperations/flashOps/TPFlashTest.java
@@ -0,0 +1,93 @@
+/**
+ * 
+ */
+package neqsim.thermodynamicOperations.flashOps;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import neqsim.thermodynamicOperations.ThermodynamicOperations;
+
+/**
+ * @author ESOL
+ *
+ */
+class TPFlashTest {
+
+  static neqsim.thermo.system.SystemInterface testSystem = null;
+  static ThermodynamicOperations testOps = null;
+
+  /**
+   * @throws java.lang.Exception
+   */
+  @BeforeEach
+  void setUp() throws Exception {
+    testSystem = new neqsim.thermo.system.SystemPrEos(243.15, 300.0);
+    testSystem.addComponent("nitrogen", 1.0);
+    testSystem.addComponent("methane", 90.0);
+    testSystem.addComponent("ethane", 2.0);
+    testSystem.addComponent("propane", 1.0);
+    testSystem.addComponent("i-butane", 1.0);
+    testSystem.addComponent("n-butane", 1.0);
+    testSystem.addComponent("i-pentane", 1.0);
+    testSystem.addComponent("n-pentane", 1.0);
+    testSystem.addComponent("n-hexane", 1.0);
+    testSystem.addComponent("nC10", 1.0);
+    testSystem.addComponent("water", 10.0);
+    testSystem.setMixingRule("classic");
+    testSystem.setMultiPhaseCheck(true);
+  }
+
+
+  @Test
+  void testRun() {
+    testSystem.setMultiPhaseCheck(true);
+    testSystem.setPressure(10.0, "bara");
+    testSystem.setTemperature(25.0, "C");
+    testOps = new ThermodynamicOperations(testSystem);
+    testOps.TPflash();
+    testSystem.initProperties();
+    double enthalpy = testSystem.getEnthalpy();
+    assertEquals(-430041.49312169873, testSystem.getEnthalpy(), 1e-2);
+  }
+  
+
+  @Test
+  void testRun2() {
+    testSystem.setMultiPhaseCheck(false);
+    testSystem.setPressure(10.0, "bara");
+    testSystem.setTemperature(25.0, "C");
+    testOps = new ThermodynamicOperations(testSystem);
+    testOps.TPflash();
+    testSystem.initProperties();
+    double enthalpy = testSystem.getEnthalpy();
+    assertEquals(-359394.2117634512, testSystem.getEnthalpy(), 1e-2);
+  }
+  
+  @Test
+  void testRun3() {
+    testSystem.setMultiPhaseCheck(false);
+    testSystem.setPressure(500.0, "bara");
+    testSystem.setTemperature(15.0, "C");
+    testOps = new ThermodynamicOperations(testSystem);
+    testOps.TPflash();
+    testSystem.initProperties();
+    double enthalpy = testSystem.getEnthalpy();
+    assertEquals(-552568.2810227782, testSystem.getEnthalpy(), 1e-2);
+  }
+  
+  //@Test
+  void testRun4() {
+    testSystem.setMultiPhaseCheck(true);
+    testSystem.setPressure(500.0, "bara");
+    testSystem.setTemperature(15.0, "C");
+    testOps = new ThermodynamicOperations(testSystem);
+    testOps.TPflash();
+    testSystem.initProperties();
+    double enthalpy = testSystem.getEnthalpy();
+    assertEquals(-936973.1969586421, testSystem.getEnthalpy(), 1e-2);
+  }
+
+}
