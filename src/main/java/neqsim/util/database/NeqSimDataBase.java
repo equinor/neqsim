7f7533d54e67c8441d17a640aeb0e9bf5f860f6f
EvenSol <esolbraa@gmail.com>
2022-05-22 10:19:32 +0200

    Update NeqSimDataBase.java

--- src/main/java/neqsim/util/database/NeqSimDataBase.java
+++ src/main/java/neqsim/util/database/NeqSimDataBase.java
@@ -48,14 +48,15 @@ public class NeqSimDataBase implements neqsim.util.util.FileSystemSettings, java
     static Logger logger = LogManager.getLogger(NeqSimDataBase.class);
     private static boolean createTemporaryTables = false;
 
-    //private static String dataBaseType = "Derby";
-    //private static String connectionString = "jdbc:derby:classpath:data/neqsimthermodatabase";
+    private static String dataBaseType = "Derby";
+    private static String connectionString = "jdbc:derby:classpath:data/neqsimthermodatabase";
     private static String username = "remote";
     private static String password = "remote";
 
-     static String dataBaseType = "MSAccessUCanAccess";
-     public static String connectionString =
-     "jdbc:ucanaccess://C:/Users/esol/OneDrive - Equinor/programming/neqsimdatabase/MSAccess/NeqSimDataBase.mdb;memory=true";
+    // static String dataBaseType = "MSAccessUCanAccess";
+    // public static String connectionString =
+    // "jdbc:ucanaccess://C:/Users/esol/OneDrive -
+    // Equinor/programming/neqsimdatabase/MSAccess/NeqSimDataBase.mdb;memory=true";
 
     private Statement statement = null;
     protected Connection databaseConnection = null;
