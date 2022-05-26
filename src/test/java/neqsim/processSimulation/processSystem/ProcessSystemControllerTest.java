c450349b1307cbe6701afe57664fcad8c5dddacc
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-05-26 22:33:20 +0200

    controller initial work (#389)
    * initial work
    
    * updates
    
    * update
    
    * Update ProcessSystemControllerTest.java
    
    * Update ProcessSystemControllerTest.java
    
    * Update ProcessSystemRunTransientTest.java
    
    * update
    
    * Update ProcessModuleBaseClass.java
    
    * update
    
    * Update ProcessSystemRunTransientTest.java
    
    * refact: rename variable, getters and setters
    refact: typo
    
    * bugfix: invert when to calculate transient as steady state
    
    * bugfix: run controller for stream if it exists
    
    * Update ProcessEquipmentBaseClass.java
    
    * refact: moved code to SimulationInterface
    
    * add cache file to gitignore
    
    * revert
    
    * revert
    git push
    
    * refact: removed unnecessary line
    
    Co-authored-by: Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>

--- src/test/java/neqsim/processSimulation/processSystem/ProcessSystemControllerTest.java
+++ src/test/java/neqsim/processSimulation/processSystem/ProcessSystemControllerTest.java
@@ -71,14 +71,14 @@ public class ProcessSystemControllerTest extends neqsim.NeqSimTest {
     VolumeFlowTransmitter flowTransmitter =
         new VolumeFlowTransmitter(separator_1.getGasOutStream());
     flowTransmitter.setUnit("kg/hr");
-    flowTransmitter.setMaximumValue(110.0);
-    flowTransmitter.setMinimumValue(90.0);
+    flowTransmitter.setMaximumValue(150.0);
+    flowTransmitter.setMinimumValue(10.0);
 
     ControllerDeviceInterface flowController = new ControllerDeviceBaseClass();
     flowController.setTransmitter(flowTransmitter);
     flowController.setReverseActing(true);
     flowController.setControllerSetPoint(65.0 + getRandomDistrurbanceFlowRate());
-    flowController.setControllerParameters(0.01, 0.10, 0.100);
+    flowController.setControllerParameters(0.5, 100.100, 0.0);
 
     p.add(stream_1);
     p.add(valve_1);
@@ -90,8 +90,31 @@ public class ProcessSystemControllerTest extends neqsim.NeqSimTest {
 
     // transient behaviour
     p.setTimeStep(1.0);
-    for (int i = 0; i < 25; i++) {
+    for (int i = 0; i < 55; i++) {
       flowController.setControllerSetPoint(65.0 + getRandomDistrurbanceFlowRate());
+      p.runTransient();
+      System.out.println(
+          "flow rate " + valve_1.getOutStream().getFluid().getPhase("gas").getFlowRate("kg/hr")
+              + " controller response " + flowController.getResponse() + " valve opening "
+              + valve_1.getPercentValveOpening() + " pressure "
+              + separator_1.getGasOutStream().getPressure());
+    }
+
+    for (int i = 0; i < 100; i++) {
+      flowController.setControllerSetPoint(55.0 + getRandomDistrurbanceFlowRate());
+      // stream_1.runTransient(1.0);
+      p.runTransient();
+      System.out.println(
+          "flow rate " + valve_1.getOutStream().getFluid().getPhase("gas").getFlowRate("kg/hr")
+              + " controller response " + flowController.getResponse() + " valve opening "
+              + valve_1.getPercentValveOpening() + " pressure "
+              + separator_1.getGasOutStream().getPressure());
+    }
+
+    // transient behaviour
+    p.setTimeStep(1.0);
+    for (int i = 0; i < 55; i++) {
+      flowController.setControllerSetPoint(75.0 + getRandomDistrurbanceFlowRate());
       p.runTransient();
       System.out.println(
           "flow rate " + valve_1.getOutStream().getFluid().getPhase("gas").getFlowRate("kg/hr")
@@ -101,5 +124,4 @@ public class ProcessSystemControllerTest extends neqsim.NeqSimTest {
       // p.runTransient();
     }
   }
-
 }
