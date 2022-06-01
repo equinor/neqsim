bd2fc3a81d259de8b21e759337b1a4d6b2a6a3e0
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-06-01 21:11:22 +0200

    revert

--- src/main/java/neqsim/processSimulation/processEquipment/subsea/SimpleFlowLine.java
+++ src/main/java/neqsim/processSimulation/processEquipment/subsea/SimpleFlowLine.java
@@ -14,105 +14,105 @@ import neqsim.thermo.system.SystemInterface;
  * @version $Id: $Id
  */
 public class SimpleFlowLine extends TwoPortEquipment {
-  private static final long serialVersionUID = 1000;
+    private static final long serialVersionUID = 1000;
 
-  private double height = 100.0;
-  public double length = 520.0;
-  double outletTemperature = 313.15;
-  AdiabaticTwoPhasePipe pipeline;
+    private double height = 100.0;
+    public double length = 520.0;
+    double outletTemperature = 313.15;
+    AdiabaticTwoPhasePipe pipeline;
 
-  /**
-   * <p>
-   * Constructor for SimpleFlowLine.
-   * </p>
-   *
-   * @param inStream a {@link neqsim.processSimulation.processEquipment.stream.StreamInterface}
-   *        object
-   */
-  @Deprecated
-  public SimpleFlowLine(StreamInterface inStream) {
-    this("SimpleFlowLine", inStream);
-
-  }
-
-  /**
-   * Constructor for SimpleFlowLine.
-   * 
-   * @param name Name of SimpleFlowLine object
-   * @param inStream a {@link neqsim.processSimulation.processEquipment.stream.StreamInterface}
-   *        object
-   */
-  public SimpleFlowLine(String name, StreamInterface inStream) {
-    super(name);
-    this.inStream = inStream;
-    setOutletStream(inStream.clone());
-    pipeline = new AdiabaticTwoPhasePipe("pipeline", inStream);
-  }
-
-  /**
-   * <p>
-   * Getter for the field <code>pipeline</code>.
-   * </p>
-   *
-   * @return a {@link neqsim.processSimulation.processEquipment.pipeline.AdiabaticTwoPhasePipe}
-   *         object
-   */
-  public AdiabaticTwoPhasePipe getPipeline() {
-    return pipeline;
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public SystemInterface getThermoSystem() {
-    return getOutletStream().getThermoSystem();
-  }
-
-  /** {@inheritDoc} */
-  @Override
-  public void run() {
-    pipeline.run();
-    getOutletStream().setFluid(pipeline.getOutletStream().getFluid());
-
-    /*
-     * System.out.println("stary P " );
-     * 
-     * SystemInterface fluidIn = (inStream.getFluid()).clone(); fluidIn.initProperties();
-     * 
-     * double density = fluidIn.getDensity("kg/m3");
-     * 
-     * double deltaP = density*getHeight()*neqsim.thermo.ThermodynamicConstantsInterface.gravity/1.
-     * 0e5;
-     * 
-     * System.out.println("density " +density + " delta P " + deltaP);
-     * 
-     * fluidIn.setPressure(fluidIn.getPressure("bara")-deltaP);
-     * fluidIn.setTemperature(outletTemperature);
-     * 
-     * ThermodynamicOperations ops = new ThermodynamicOperations(fluidIn); ops.TPflash();
-     * 
-     * getOutletStream().setFluid(fluidIn);
+    /**
+     * <p>
+     * Constructor for SimpleFlowLine.
+     * </p>
+     *
+     * @param inStream a {@link neqsim.processSimulation.processEquipment.stream.StreamInterface}
+     *        object
      */
-  }
+    @Deprecated
+    public SimpleFlowLine(StreamInterface inStream) {
+        this("SimpleFlowLine", inStream);
 
-  /**
-   * <p>
-   * Getter for the field <code>height</code>.
-   * </p>
-   *
-   * @return a double
-   */
-  public double getHeight() {
-    return height;
-  }
+    }
 
-  /**
-   * <p>
-   * Setter for the field <code>height</code>.
-   * </p>
-   *
-   * @param height a double
-   */
-  public void setHeight(double height) {
-    this.height = height;
-  }
+    /**
+     * Constructor for SimpleFlowLine.
+     * 
+     * @param name Name of SimpleFlowLine object
+     * @param inStream a {@link neqsim.processSimulation.processEquipment.stream.StreamInterface}
+     *        object
+     */
+    public SimpleFlowLine(String name, StreamInterface inStream) {
+        super(name);
+        this.inStream = inStream;
+        setOutletStream(inStream.clone());
+        pipeline = new AdiabaticTwoPhasePipe("pipeline", inStream);
+    }
+
+    /**
+     * <p>
+     * Getter for the field <code>pipeline</code>.
+     * </p>
+     *
+     * @return a {@link neqsim.processSimulation.processEquipment.pipeline.AdiabaticTwoPhasePipe}
+     *         object
+     */
+    public AdiabaticTwoPhasePipe getPipeline() {
+        return pipeline;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public SystemInterface getThermoSystem() {
+      return getOutletStream().getThermoSystem();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public void run() {
+        pipeline.run();
+        getOutletStream().setFluid(pipeline.getOutletStream().getFluid());
+
+        /*
+         * System.out.println("stary P " );
+         * 
+         * SystemInterface fluidIn = (inStream.getFluid()).clone(); fluidIn.initProperties();
+         * 
+         * double density = fluidIn.getDensity("kg/m3");
+         * 
+         * double deltaP =
+         * density*getHeight()*neqsim.thermo.ThermodynamicConstantsInterface.gravity/1. 0e5;
+         * 
+         * System.out.println("density " +density + " delta P " + deltaP);
+         * 
+         * fluidIn.setPressure(fluidIn.getPressure("bara")-deltaP);
+         * fluidIn.setTemperature(outletTemperature);
+         * 
+         * ThermodynamicOperations ops = new ThermodynamicOperations(fluidIn); ops.TPflash();
+         * 
+         * getOutletStream().setFluid(fluidIn);
+         */
+    }
+
+    /**
+     * <p>
+     * Getter for the field <code>height</code>.
+     * </p>
+     *
+     * @return a double
+     */
+    public double getHeight() {
+        return height;
+    }
+
+    /**
+     * <p>
+     * Setter for the field <code>height</code>.
+     * </p>
+     *
+     * @param height a double
+     */
+    public void setHeight(double height) {
+        this.height = height;
+    }
 }
