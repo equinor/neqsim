f46b673c3433b102b4ab7676bb9be7c0f3b382ad
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2022-06-23 14:03:19 +0200

    refact: formatting and style warnings

--- src/main/java/neqsim/processSimulation/mechanicalDesign/designStandards/MaterialPlateDesignStandard.java
+++ src/main/java/neqsim/processSimulation/mechanicalDesign/designStandards/MaterialPlateDesignStandard.java
@@ -11,108 +11,105 @@ import neqsim.processSimulation.mechanicalDesign.MechanicalDesign;
  * @version $Id: $Id
  */
 public class MaterialPlateDesignStandard extends DesignStandard {
-    private static final long serialVersionUID = 1000;
+  private static final long serialVersionUID = 1000;
 
-    /**
-     * <p>
-     * Constructor for MaterialPlateDesignStandard.
-     * </p>
-     */
-    public MaterialPlateDesignStandard() {}
+  /**
+   * <p>
+   * Constructor for MaterialPlateDesignStandard.
+   * </p>
+   */
+  public MaterialPlateDesignStandard() {}
 
-    /**
-     * <p>
-     * Constructor for MaterialPlateDesignStandard.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign}
-     *        object
-     */
-    public MaterialPlateDesignStandard(String name, MechanicalDesign equipmentInn) {
-        super(name, equipmentInn);
-        readMaterialDesignStandard("Carbon Steel Plates and Sheets", "SA-516", "55", 1);
-    }
+  /**
+   * <p>
+   * Constructor for MaterialPlateDesignStandard.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign} object
+   */
+  public MaterialPlateDesignStandard(String name, MechanicalDesign equipmentInn) {
+    super(name, equipmentInn);
+    readMaterialDesignStandard("Carbon Steel Plates and Sheets", "SA-516", "55", 1);
+  }
 
-    /**
-     * <p>
-     * Getter for the field <code>divisionClass</code>.
-     * </p>
-     *
-     * @return the divisionClass
-     */
-    public double getDivisionClass() {
-        return divisionClass;
-    }
+  /**
+   * <p>
+   * Getter for the field <code>divisionClass</code>.
+   * </p>
+   *
+   * @return the divisionClass
+   */
+  public double getDivisionClass() {
+    return divisionClass;
+  }
 
-    /**
-     * <p>
-     * Setter for the field <code>divisionClass</code>.
-     * </p>
-     *
-     * @param divisionClass the divisionClass to set
-     */
-    public void setDivisionClass(double divisionClass) {
-        this.divisionClass = divisionClass;
-    }
+  /**
+   * <p>
+   * Setter for the field <code>divisionClass</code>.
+   * </p>
+   *
+   * @param divisionClass the divisionClass to set
+   */
+  public void setDivisionClass(double divisionClass) {
+    this.divisionClass = divisionClass;
+  }
 
-    String grade = "";
+  String grade = "";
 
-    String materialName = "";
-    String specificationNumber = "";
-    int divisionClassNumber = 1;
-    private double divisionClass = 425;
+  String materialName = "";
+  String specificationNumber = "";
+  int divisionClassNumber = 1;
+  private double divisionClass = 425;
 
-    /**
-     * <p>
-     * readMaterialDesignStandard.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @param specNo a {@link java.lang.String} object
-     * @param grade a {@link java.lang.String} object
-     * @param divClassNo a int
-     */
-    public void readMaterialDesignStandard(String name, String specNo, String grade,
-            int divClassNo) {
-        materialName = name;
-        specificationNumber = specNo;
-        divisionClassNumber = divClassNo;
+  /**
+   * <p>
+   * readMaterialDesignStandard.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param specNo a {@link java.lang.String} object
+   * @param grade a {@link java.lang.String} object
+   * @param divClassNo a int
+   */
+  public void readMaterialDesignStandard(String name, String specNo, String grade, int divClassNo) {
+    materialName = name;
+    specificationNumber = specNo;
+    divisionClassNumber = divClassNo;
 
-        neqsim.util.database.NeqSimTechnicalDesignDatabase database =
-                new neqsim.util.database.NeqSimTechnicalDesignDatabase();
-        java.sql.ResultSet dataSet = null;
-        try {
-            try {
-                dataSet = database
-                        .getResultSet(("SELECT * FROM materialplateproperties WHERE materialName='"
-                                + name + "' AND grade='" + grade + "' AND specificationNumber='"
-                                + specNo + "'"));
-                while (dataSet.next()) {
-                    if (divClassNo == 1) {
-                        divisionClass = (Double.parseDouble(dataSet.getString("divisionClass1")))
-                                * 0.00689475729; // MPa
-                    } else {
-                        divisionClass = (Double.parseDouble(dataSet.getString("divisionClass2")))
-                                * 0.00689475729; // MPa
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            // gasLoadFactor = Double.parseDouble(dataSet.getString("gasloadfactor"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (dataSet != null) {
-                    dataSet.close();
-                }
-            } catch (Exception e) {
-                System.out.println("error closing database.....GasScrubberDesignStandard");
-                e.printStackTrace();
-            }
+    neqsim.util.database.NeqSimTechnicalDesignDatabase database =
+        new neqsim.util.database.NeqSimTechnicalDesignDatabase();
+    java.sql.ResultSet dataSet = null;
+    try {
+      try {
+        dataSet =
+            database.getResultSet(("SELECT * FROM materialplateproperties WHERE materialName='"
+                + name + "' AND grade='" + grade + "' AND specificationNumber='" + specNo + "'"));
+        while (dataSet.next()) {
+          if (divClassNo == 1) {
+            divisionClass =
+                (Double.parseDouble(dataSet.getString("divisionClass1"))) * 0.00689475729; // MPa
+          } else {
+            divisionClass =
+                (Double.parseDouble(dataSet.getString("divisionClass2"))) * 0.00689475729; // MPa
+          }
         }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+
+      // gasLoadFactor = Double.parseDouble(dataSet.getString("gasloadfactor"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (dataSet != null) {
+          dataSet.close();
+        }
+      } catch (Exception e) {
+        System.out.println("error closing database.....GasScrubberDesignStandard");
+        e.printStackTrace();
+      }
     }
+  }
 }
--- src/main/java/neqsim/processSimulation/mechanicalDesign/designStandards/SeparatorDesignStandard.java
+++ src/main/java/neqsim/processSimulation/mechanicalDesign/designStandards/SeparatorDesignStandard.java
@@ -12,151 +12,149 @@ import neqsim.processSimulation.processEquipment.separator.SeparatorInterface;
  * @version $Id: $Id
  */
 public class SeparatorDesignStandard extends DesignStandard {
-    private static final long serialVersionUID = 1000;
+  private static final long serialVersionUID = 1000;
 
-    /**
-     * <p>
-     * getFg.
-     * </p>
-     *
-     * @return the Fg
-     */
-    public double getFg() {
-        return Fg;
-    }
+  /**
+   * <p>
+   * getFg.
+   * </p>
+   *
+   * @return the Fg
+   */
+  public double getFg() {
+    return Fg;
+  }
 
-    /**
-     * <p>
-     * setFg.
-     * </p>
-     *
-     * @param Fg the Fg to set
-     */
-    public void setFg(double Fg) {
-        this.Fg = Fg;
-    }
+  /**
+   * <p>
+   * setFg.
+   * </p>
+   *
+   * @param Fg the Fg to set
+   */
+  public void setFg(double Fg) {
+    this.Fg = Fg;
+  }
 
-    double gasLoadFactor = 0.11;
-    private double Fg = 0.8;
-    private double volumetricDesignFactor = 1.0;
+  double gasLoadFactor = 0.11;
+  private double Fg = 0.8;
+  private double volumetricDesignFactor = 1.0;
 
-    /**
-     * <p>
-     * Constructor for SeparatorDesignStandard.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign}
-     *        object
-     */
-    public SeparatorDesignStandard(String name, MechanicalDesign equipmentInn) {
-        super(name, equipmentInn);
-        neqsim.util.database.NeqSimTechnicalDesignDatabase database =
-                new neqsim.util.database.NeqSimTechnicalDesignDatabase();
-        java.sql.ResultSet dataSet = null;
-        try {
-            try {
-                dataSet = database.getResultSet(
-                        ("SELECT * FROM technicalrequirements_process WHERE EQUIPMENTTYPE='Separator' AND Company='"
-                                + standardName + "'"));
-                while (dataSet.next()) {
-                    String specName = dataSet.getString("SPECIFICATION");
-                    if (specName.equals("GasLoadFactor")) {
-                        gasLoadFactor = (Double.parseDouble(dataSet.getString("MAXVALUE"))
-                                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
-                    }
-                    if (specName.equals("Fg")) {
-                        Fg = (Double.parseDouble(dataSet.getString("MAXVALUE"))
-                                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
-                    }
-                    if (specName.equals("VolumetricDesignFactor")) {
-                        volumetricDesignFactor = (Double.parseDouble(dataSet.getString("MAXVALUE"))
-                                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
-                    }
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-
-            // gasLoadFactor = Double.parseDouble(dataSet.getString("gasloadfactor"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                if (dataSet != null) {
-                    dataSet.close();
-                }
-            } catch (Exception e) {
-                System.out.println("error closing database.....GasScrubberDesignStandard");
-                e.printStackTrace();
-            }
+  /**
+   * <p>
+   * Constructor for SeparatorDesignStandard.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign} object
+   */
+  public SeparatorDesignStandard(String name, MechanicalDesign equipmentInn) {
+    super(name, equipmentInn);
+    neqsim.util.database.NeqSimTechnicalDesignDatabase database =
+        new neqsim.util.database.NeqSimTechnicalDesignDatabase();
+    java.sql.ResultSet dataSet = null;
+    try {
+      try {
+        dataSet = database.getResultSet(
+            ("SELECT * FROM technicalrequirements_process WHERE EQUIPMENTTYPE='Separator' AND Company='"
+                + standardName + "'"));
+        while (dataSet.next()) {
+          String specName = dataSet.getString("SPECIFICATION");
+          if (specName.equals("GasLoadFactor")) {
+            gasLoadFactor = (Double.parseDouble(dataSet.getString("MAXVALUE"))
+                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
+          }
+          if (specName.equals("Fg")) {
+            Fg = (Double.parseDouble(dataSet.getString("MAXVALUE"))
+                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
+          }
+          if (specName.equals("VolumetricDesignFactor")) {
+            volumetricDesignFactor = (Double.parseDouble(dataSet.getString("MAXVALUE"))
+                + Double.parseDouble(dataSet.getString("MINVALUE"))) / 2.0;
+          }
         }
-    }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
 
-    /**
-     * <p>
-     * Getter for the field <code>gasLoadFactor</code>.
-     * </p>
-     *
-     * @return a double
-     */
-    public double getGasLoadFactor() {
-        if (standardName.equals("StatoilTR")) {
-            return gasLoadFactor;
-        } else {
-            return gasLoadFactor;
+      // gasLoadFactor = Double.parseDouble(dataSet.getString("gasloadfactor"));
+    } catch (Exception e) {
+      e.printStackTrace();
+    } finally {
+      try {
+        if (dataSet != null) {
+          dataSet.close();
         }
+      } catch (Exception e) {
+        System.out.println("error closing database.....GasScrubberDesignStandard");
+        e.printStackTrace();
+      }
     }
+  }
 
-    /**
-     * <p>
-     * Getter for the field <code>volumetricDesignFactor</code>.
-     * </p>
-     *
-     * @return a double
-     */
-    public double getVolumetricDesignFactor() {
-        if (standardName.equals("StatoilTR")) {
-            return volumetricDesignFactor;
-        } else {
-            return volumetricDesignFactor;
-        }
+  /**
+   * <p>
+   * Getter for the field <code>gasLoadFactor</code>.
+   * </p>
+   *
+   * @return a double
+   */
+  public double getGasLoadFactor() {
+    if (standardName.equals("StatoilTR")) {
+      return gasLoadFactor;
+    } else {
+      return gasLoadFactor;
     }
+  }
 
-    /**
-     * <p>
-     * Setter for the field <code>volumetricDesignFactor</code>.
-     * </p>
-     *
-     * @param volumetricDesignFactor the volumetricDesignFactor to set
-     */
-    public void setVolumetricDesignFactor(double volumetricDesignFactor) {
-        this.volumetricDesignFactor = volumetricDesignFactor;
+  /**
+   * <p>
+   * Getter for the field <code>volumetricDesignFactor</code>.
+   * </p>
+   *
+   * @return a double
+   */
+  public double getVolumetricDesignFactor() {
+    if (standardName.equals("StatoilTR")) {
+      return volumetricDesignFactor;
+    } else {
+      return volumetricDesignFactor;
     }
+  }
 
-    /**
-     * <p>
-     * getLiquidRetentionTime.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign}
-     *        object
-     * @return a double
-     */
-    public double getLiquidRetentionTime(String name, MechanicalDesign equipmentInn) {
-        double retTime = 90.0;
-        double dens = ((SeparatorInterface) equipmentInn.getProcessEquipment()).getThermoSystem()
-                .getPhase(1).getPhysicalProperties().getDensity() / 1000.0;
+  /**
+   * <p>
+   * Setter for the field <code>volumetricDesignFactor</code>.
+   * </p>
+   *
+   * @param volumetricDesignFactor the volumetricDesignFactor to set
+   */
+  public void setVolumetricDesignFactor(double volumetricDesignFactor) {
+    this.volumetricDesignFactor = volumetricDesignFactor;
+  }
 
-        // select correct residensetime from database
-        // to be implmented
-        if (name.equals("API12J")) {
-            if (dens < 0.85)
-                retTime = 60.0;
-            else if (dens > 0.93)
-                retTime = 180.0;
-        }
-        return retTime;
+  /**
+   * <p>
+   * getLiquidRetentionTime.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param equipmentInn a {@link neqsim.processSimulation.mechanicalDesign.MechanicalDesign} object
+   * @return a double
+   */
+  public double getLiquidRetentionTime(String name, MechanicalDesign equipmentInn) {
+    double retTime = 90.0;
+    double dens = ((SeparatorInterface) equipmentInn.getProcessEquipment()).getThermoSystem()
+        .getPhase(1).getPhysicalProperties().getDensity() / 1000.0;
+
+    // select correct residensetime from database
+    // to be implmented
+    if (name.equals("API12J")) {
+      if (dens < 0.85)
+        retTime = 60.0;
+      else if (dens > 0.93)
+        retTime = 180.0;
     }
+    return retTime;
+  }
 }
