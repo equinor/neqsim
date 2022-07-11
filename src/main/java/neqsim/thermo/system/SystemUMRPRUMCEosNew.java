dd97678c17f3d650ae559bc8338a43820356c97c
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-07-11 23:26:23 +0200

    Fixjavadoc1 (#465)
    * fix javadoc
    
    * fixed flyid system doc

--- src/main/java/neqsim/thermo/system/SystemUMRPRUMCEosNew.java
+++ src/main/java/neqsim/thermo/system/SystemUMRPRUMCEosNew.java
@@ -6,7 +6,8 @@
 
 package neqsim.thermo.system;
 /**
- *
+ * This class defines a thermodynamic system using the UMR-PRU with MC paramters equation of state
+ * 
  * @author Even Solbraa
  * @version
  */
