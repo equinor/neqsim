9fc3f66595d961bbf92d1be6a0ead46d66173f34
netaji2022 <108813394+netaji2022@users.noreply.github.com>
2022-07-13 08:28:18 +0000

    Added 1st class and constructor

---
+++ src/main/java/neqsim/physicalProperties/physicalPropertyMethods/commonPhasePhysicalProperties/viscosity/SuperTrappViscosity.java
@@ -0,0 +1,16 @@
+package neqsim.physicalProperties.physicalPropertyMethods.commonPhasePhysicalProperties.viscosity;
+import neqsim.thermo.system.SystemInterface;
+import neqsim.thermo.system.SystemSrkEos;
+
+public class SuperTrappViscosity extends Viscosity {
+
+
+
+  public SuperTrappViscosity() {
+
+
+
+  }
+
+}
+
