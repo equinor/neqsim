fe17d824bf5d6a5fabe8e7e388419727e1eb571d
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2022-09-30 14:27:54 +0000

    implementation started

--- src/main/java/neqsim/thermodynamicOperations/phaseEnvelopeOps/multicomponentEnvelopeOps/pTphaseEnvelopeHCwater.java
+++ src/main/java/neqsim/thermodynamicOperations/phaseEnvelopeOps/multicomponentEnvelopeOps/pTphaseEnvelopeHCwater.java
@@ -9,6 +9,7 @@ package neqsim.thermodynamicOperations.phaseEnvelopeOps.multicomponentEnvelopeOp
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import neqsim.thermo.system.SystemInterface;
+import neqsim.thermodynamicOperations.ThermodynamicOperations;
 
 /**
  * <p>
@@ -33,6 +34,8 @@ public class pTphaseEnvelopeHCwater extends pTphaseEnvelope {
    * <p>
    * Constructor for pTphaseEnvelopeHCwater.
    * </p>
+   * Method follow algorithm in : Lindeloff, N. and M.L. Michelsen, Phase envelope calculations for
+   * hydrocarbon-water mixtures. SPE Journal, 2003. 8(03): p. 298-303.
    *
    * @param system a {@link neqsim.thermo.system.SystemInterface} object
    * @param name a {@link java.lang.String} object
@@ -48,12 +51,49 @@ public class pTphaseEnvelopeHCwater extends pTphaseEnvelope {
   /** {@inheritDoc} */
   @Override
   public void run() {
-   //to be implemented.....
+    // envelope contruction method comes here.....
 
-   //envelope contruction method comes here.....
+    /**
+     * Steps in algorithm (figure names references original paper of Lindeloff and Michelsen):
+     * 
+     * 
+     * Tracing the dewline segments that separate the single-phase region from the two-phase region
+     * (Lines I and II in Fig. 1).
+     * 
+     * Determining the three-phase points on the dewline. These are the points where the overall
+     * composition is at equilibrium with two incipient phases.
+     * 
+     * Tracing the phase boundaries that separate the two-phase region from the three-phase region
+     * (Lines III and IV in Fig. 1). These phase boundaries emerge from the three-phase points or
+     * exist as isolated lines.
+     * 
+     */
+
+
+    // Step 1Tracing the dewline segments that separate the single-phase region from the
+    // two-phase region (Lines I and II in Fig. 1).
+    //
+    // setting pressure to 1.0 atm
+    system.setPressure(50.0, "atm");
+    // set temperature to 0C
+    system.setTemperature(0.0, "C");
+    // initializing fluid and calculaton K-values accirding to Wilson
+    system.init(0);
+    // Create a thermodynamic operation object for the fluid
+    ThermodynamicOperations ops = new ThermodynamicOperations(system);
+    // Calculate the dew point temprature of the fluid
+    try {
+      ops.dewPointTemperatureFlash(false);
+    } catch (Exception e) {
+      logger.error(e.toString());
+    }
+    double dewPointTemperature = system.getTemperature("C");
+    String dewPhaseType = system.getPhase(1).getPhaseTypeName();
+    double dewPointPressure = system.getPressure("bara");
+
 
     //at the moment we use the HC phase envelope method in the super class
-   super.run();
+    // super.run();
   }
 
 }
--- src/test/java/neqsim/thermodynamicOperations/phaseEnvelopeOps/multicomponentEnvelopeOps/pTphaseEnvelopeHCwaterTest.java
+++ src/test/java/neqsim/thermodynamicOperations/phaseEnvelopeOps/multicomponentEnvelopeOps/pTphaseEnvelopeHCwaterTest.java
@@ -2,30 +2,64 @@ package neqsim.thermodynamicOperations.phaseEnvelopeOps.multicomponentEnvelopeOp
 
 import org.junit.jupiter.api.Test;
 import neqsim.thermo.system.SystemInterface;
+import neqsim.thermo.system.SystemSrkCPAstatoil;
 import neqsim.thermo.system.SystemSrkEos;
 import neqsim.thermodynamicOperations.ThermodynamicOperations;
 
+/**
+ * Tests for phase envelop algorithm as presented in: Lindeloff, N. and M.L. Michelsen, Phase
+ * envelope calculations for hydrocarbon-water mixtures. SPE Journal, 2003. 8(03): p. 298-303.
+ *
+ **/
 public class pTphaseEnvelopeHCwaterTest {
   static SystemInterface thermoSystem = null;
 
+  /**
+   * In the currecnt test Fluid1 from the paper is tested.
+   **/
   @Test
-  void testRun() {
+  void testFluid1() {
     thermoSystem = new SystemSrkEos(298.0, 10.0);
-    thermoSystem.addComponent("methane", 51.0);
-    thermoSystem.addComponent("ethane", 1.0);
-    thermoSystem.addComponent("propane", 1.0);
-    thermoSystem.addTBPfraction("C6", 1.0, 90.0 / 1000.0, 0.7);
-    thermoSystem.addTBPfraction("C7", 1.0, 110.0 / 1000.0, 0.73);
-    thermoSystem.addTBPfraction("C8", 1.0, 120.0 / 1000.0, 0.76);
-    thermoSystem.addTBPfraction("C9", 1.0, 140.0 / 1000.0, 0.79);
-    thermoSystem.addTBPfraction("C10", 11.0, 290.0 / 1000.0, 0.82);
-
-    // at the moment water is not handeled in the routine......
-    // thermoSystem.addComponent("water", 10.0);
+    thermoSystem.addComponent("nitrogen", 0.34);
+    thermoSystem.addComponent("CO2", 0.84);
+    thermoSystem.addComponent("methane", 89.95);
+    thermoSystem.addComponent("ethane", 5.17);
+    thermoSystem.addComponent("propane", 2.04);
+    thermoSystem.addComponent("i-butane", 0.36);
+    thermoSystem.addComponent("n-butane", 0.55);
+    thermoSystem.addComponent("i-pentane", 0.14);
+    thermoSystem.addComponent("n-pentane", 0.10);
+    thermoSystem.addComponent("n-hexane", 0.01);
+    thermoSystem.addComponent("water", 500e-4);
+    thermoSystem.setMixingRule("classic");
 
     ThermodynamicOperations testOps = new ThermodynamicOperations(thermoSystem);
     testOps.calcPTphaseEnvelopeHCwater();
 
+  }
+
+  /**
+   * In the currecnt test Fluid2 from the paper is tested.
+   **/
+  @Test
+  void testFluid2() {
+    thermoSystem = new SystemSrkCPAstatoil(298.0, 10.0);
+    thermoSystem.addComponent("CO2", 2.79);
+    thermoSystem.addComponent("methane", 71.51);
+    thermoSystem.addComponent("ethane", 5.77);
+    thermoSystem.addComponent("propane", 4.10);
+    thermoSystem.addComponent("i-butane", 1.32);
+    thermoSystem.addComponent("n-butane", 1.60);
+    thermoSystem.addComponent("i-pentane", 0.82);
+    thermoSystem.addComponent("n-pentane", 0.64);
+    thermoSystem.addComponent("n-hexane", 1.05);
+    thermoSystem.addTBPfraction("C7", 10.40, 191.0 / 1.0e3, 0.82);
+
+    thermoSystem.addComponent("water", 8.0);
+    thermoSystem.setMixingRule(10);
+
+    ThermodynamicOperations testOps = new ThermodynamicOperations(thermoSystem);
+    testOps.calcPTphaseEnvelopeHCwater();
 
   }
 }
