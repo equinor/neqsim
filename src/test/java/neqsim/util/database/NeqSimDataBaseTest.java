7f1b6e910179ff4fe1d963b4c33b52242952b863
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-04-15 16:30:34 +0000

    add test

---
+++ src/test/java/neqsim/util/database/NeqSimDataBaseTest.java
@@ -0,0 +1,24 @@
+package neqsim.util.database;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import neqsim.NeqSimTest;
+
+public class NeqSimDataBaseTest extends NeqSimTest {
+  @Test
+  void testHasComponent() {
+    assertTrue(neqsim.util.database.NeqSimDataBase.hasComponent("methane"),
+        "Could not load component methane");
+  }
+
+  @Test
+  void testInitH2DatabaseFromCSVfiles() {
+    neqsim.util.database.NeqSimDataBase.initH2DatabaseFromCSVfiles();
+  }
+
+  @Test
+  void testUpdateTable() {
+    neqsim.util.database.NeqSimDataBase.updateTable("COMP",
+        "/workspaces/neqsim/src/main/resources/data/COMP.csv");
+  }
+}
