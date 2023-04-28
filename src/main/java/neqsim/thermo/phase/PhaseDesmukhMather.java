7d80eab8eb496beafe9181cf66c0435da6e85a04
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-04-28 23:55:53 +0200

    more

--- src/main/java/neqsim/thermo/phase/PhaseDesmukhMather.java
+++ src/main/java/neqsim/thermo/phase/PhaseDesmukhMather.java
@@ -97,7 +97,6 @@ public class PhaseDesmukhMather extends PhaseGE {
             logger.error(ex.toString());
           }
         }
-
       }
     } catch (Exception ex) {
       logger.error(ex.toString());
