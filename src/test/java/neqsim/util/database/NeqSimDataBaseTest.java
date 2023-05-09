7f838f21476248a516643f400af066aca1aecfc7
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-05-09 16:02:59 +0200

    chore: move main methods to test methods.

--- src/test/java/neqsim/util/database/NeqSimDataBaseTest.java
+++ src/test/java/neqsim/util/database/NeqSimDataBaseTest.java
@@ -1,6 +1,9 @@
 package neqsim.util.database;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.sql.ResultSet;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import neqsim.NeqSimTest;
 
@@ -15,5 +18,25 @@ public class NeqSimDataBaseTest extends NeqSimTest {
   void testUpdateTable() {
     neqsim.util.database.NeqSimDataBase.updateTable("COMP",
         "classpath:/data/COMP.csv");
+  }
+
+  @Disabled
+  @Test
+  void testMain() {
+    boolean failed = true;
+    // NeqSimDataBase.initH2DatabaseFromCSVfiles();
+    // NeqSimDataBase.initDatabaseFromCSVfiles();
+    NeqSimDataBase.updateTable("COMP", "/workspaces/neqsim/src/main/resources/data/COMP.csv");
+
+    try (NeqSimDataBase database = new NeqSimDataBase();
+        ResultSet dataSet = database.getResultSet("SELECT * FROM comp WHERE NAME='methane'")) {
+      dataSet.next();
+      System.out.println("dataset " + dataSet.getString("molarmass"));
+      dataSet.close();
+      failed = false;
+    } catch (Exception ex) {
+      System.out.println("failed ");
+    }
+    Assertions.assertFalse(failed, "Failed getting data from NeqsimDataBase");
   }
 }
