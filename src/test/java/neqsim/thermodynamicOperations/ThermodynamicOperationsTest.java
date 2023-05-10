b94dd60520e73f7e9d85f77434d173a18498ce2f
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-05-10 14:32:32 +0200

    style

--- src/test/java/neqsim/thermodynamicOperations/ThermodynamicOperationsTest.java
+++ src/test/java/neqsim/thermodynamicOperations/ThermodynamicOperationsTest.java
@@ -71,7 +71,6 @@ public class ThermodynamicOperationsTest extends neqsim.NeqSimTest {
     Assertions.assertEquals(thermoSystem.getTemperature("K"), res.fluidProperties[0][2],
         "Temperature mismatch");
 
-    // Test that
     CalculationResult res2 = thermoOps.propertyFlash(jP, jT, 0, null, null);
     Assertions.assertEquals(res2.calculationError[0],
         "neqsim.util.exception.InvalidInputException: ThermodynamicOperations:propertyFlash - Input FlashMode must be 1, 2 or 3");
