74100c42248e4e4e4098b0aee2919848dba2bf57
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-05-10 16:34:26 +0200

    Test: add tests for Phaseaddcomponent (#677)

---
+++ src/test/java/neqsim/thermo/phase/PhaseGEUnifacTest.java
@@ -0,0 +1,25 @@
+package neqsim.thermo.phase;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class PhaseGEUnifacTest {
+  static PhaseGEUnifac p;
+
+  @BeforeEach
+  void setUp() {
+    p = new PhaseGEUnifac();
+  }
+
+  @Test
+  void testAddcomponent() {
+    Assertions.assertEquals(0, p.getNumberOfComponents());
+
+    p.addComponent("ethane", 0, 0, 0);
+    Assertions.assertEquals(1, p.getNumberOfComponents());
+
+    p.addComponent("methane", 0, 0, 0);
+    Assertions.assertEquals(2, p.getNumberOfComponents());
+  }
+}
