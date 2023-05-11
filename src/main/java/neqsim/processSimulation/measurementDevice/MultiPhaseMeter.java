a08ccd5ceb45948c5909ad960fbc1583b2b8d63f
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-05-11 13:15:58 +0200

    test: convert main to test

--- src/main/java/neqsim/processSimulation/measurementDevice/MultiPhaseMeter.java
+++ src/main/java/neqsim/processSimulation/measurementDevice/MultiPhaseMeter.java
@@ -2,10 +2,8 @@ package neqsim.processSimulation.measurementDevice;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import neqsim.processSimulation.processEquipment.stream.Stream;
 import neqsim.processSimulation.processEquipment.stream.StreamInterface;
 import neqsim.thermo.system.SystemInterface;
-import neqsim.thermo.system.SystemSrkEos;
 import neqsim.thermodynamicOperations.ThermodynamicOperations;
 
 /**
@@ -98,8 +96,8 @@ public class MultiPhaseMeter extends StreamMeasurementDeviceBaseClass {
 
   /** {@inheritDoc} */
   @Override
-  public double getMeasuredValue() {
-    return stream.getThermoSystem().getFlowRate("kg/hr");
+  public double getMeasuredValue(String unit) {
+    return stream.getThermoSystem().getFlowRate(unit);
   }
 
   /**
@@ -200,46 +198,5 @@ public class MultiPhaseMeter extends StreamMeasurementDeviceBaseClass {
       logger.warn("Measurement type " + measurement + " is not found");
       return 0.0;
     }
-  }
-
-  /**
-   * <p>
-   * main.
-   * </p>
-   *
-   * @param args an array of {@link java.lang.String} objects
-   */
-  public static void main(String[] args) {
-    SystemInterface testFluid = new SystemSrkEos(338.15, 50.0);
-    testFluid.addComponent("nitrogen", 1.205);
-    testFluid.addComponent("CO2", 1.340);
-    testFluid.addComponent("methane", 87.974);
-    testFluid.addComponent("ethane", 5.258);
-    testFluid.addComponent("propane", 3.283);
-    testFluid.addComponent("i-butane", 0.082);
-    testFluid.addComponent("n-butane", 0.487);
-    testFluid.addComponent("i-pentane", 0.056);
-    testFluid.addComponent("n-pentane", 1.053);
-    testFluid.addComponent("nC10", 4.053);
-    testFluid.setMixingRule(2);
-    testFluid.setMultiPhaseCheck(true);
-
-    testFluid.setTemperature(24.0, "C");
-    testFluid.setPressure(48.0, "bara");
-    testFluid.setTotalFlowRate(4.5, "MSm3/day");
-
-    Stream stream_1 = new Stream("Stream1", testFluid);
-
-    MultiPhaseMeter multiPhaseMeter = new MultiPhaseMeter("test", stream_1);
-    multiPhaseMeter.setTemperature(90.0, "C");
-    multiPhaseMeter.setPressure(60.0, "bara");
-
-    neqsim.processSimulation.processSystem.ProcessSystem operations =
-        new neqsim.processSimulation.processSystem.ProcessSystem();
-    operations.add(stream_1);
-    operations.add(multiPhaseMeter);
-    operations.run();
-    System.out.println("GOR " + multiPhaseMeter.getMeasuredValue("GOR"));
-    System.out.println("GOR_std " + multiPhaseMeter.getMeasuredValue("GOR_std"));
   }
 }
