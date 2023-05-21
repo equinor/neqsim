e5cd75873fc4d9aff83be53dcf97d932f96848a6
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-05-21 07:59:46 +0000

    copy phasetype object in clone method

--- src/test/java/neqsim/processSimulation/measurementDevice/pHProbeTest.java
+++ src/test/java/neqsim/processSimulation/measurementDevice/pHProbeTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import neqsim.processSimulation.processEquipment.stream.Stream;
 import neqsim.processSimulation.processEquipment.stream.StreamInterface;
