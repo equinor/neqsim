f503a67aa15f6d13978fcd79f1b9d96a30c6c7b9
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-06-19 20:11:48 +0200

    refact: reuse code from constructor in init++

---
+++ src/main/java/neqsim/util/StackTraceCleaner.java
@@ -0,0 +1,23 @@
+package neqsim.util;
+
+import java.util.ArrayList;
+
+public class StackTraceCleaner {
+
+  // e.setStackTrace(StackTraceCleaner.cleanStackTrace(e.getStackTrace(), "neqsim"));
+
+
+  public static StackTraceElement[] cleanStackTrace(StackTraceElement[] st, String packageRoot) {
+    ArrayList<StackTraceElement> list = new ArrayList<>(0);
+
+    for (StackTraceElement el : st) {
+      if (el.getClassName().startsWith(packageRoot)) {
+        list.add(el);
+      }
+    }
+
+    StackTraceElement[] filtered_st = new StackTraceElement[list.size()];
+
+    return list.toArray(filtered_st);
+  }
+}
