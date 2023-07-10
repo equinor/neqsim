8befb93159867a1eb11553d04a1c64a710356d56
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-07-10 21:13:42 +0000

    update test

--- src/test/java/neqsim/processSimulation/processEquipment/pipeline/BeggsAndBrillsPipeTest.java
+++ src/test/java/neqsim/processSimulation/processEquipment/pipeline/BeggsAndBrillsPipeTest.java
@@ -1,24 +1,43 @@
 package neqsim.processSimulation.processEquipment.pipeline;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import neqsim.thermodynamicOperations.ThermodynamicOperations;
 
 public class BeggsAndBrillsPipeTest {
   @Test
-  public void testMain() {
+  public void testFlowNoVolumeCorrection() {
     neqsim.thermo.system.SystemInterface testSystem =
         new neqsim.thermo.system.SystemSrkEos((273.15 + 15), 1.01325);
 
     testSystem.addComponent("nC10", 50, "MSm^3/day");
     testSystem.setMixingRule(2);
     testSystem.init(0);
+    testSystem.useVolumeCorrection(false);
 
     ThermodynamicOperations testOps = new ThermodynamicOperations(testSystem);
     testOps.TPflash();
     testSystem.initPhysicalProperties();
 
-    //System.out.println("Oil flow rate m3/hr: " + testSystem.getPhase("oil").getFlowRate("m3/hr"));
-    System.out.println("Total flow rate m3/hr: " + testSystem.getFlowRate("m3/hr"));
-
+    Assertions.assertEquals(testSystem.getPhase("oil").getFlowRate("m3/hr"),
+        testSystem.getFlowRate("m3/hr"), 1e-4);
   }
+
+  public void testFlowVolumeCorrection() {
+    neqsim.thermo.system.SystemInterface testSystem =
+        new neqsim.thermo.system.SystemSrkEos((273.15 + 15), 1.01325);
+
+    testSystem.addComponent("nC10", 50, "MSm^3/day");
+    testSystem.setMixingRule(2);
+    testSystem.init(0);
+    testSystem.useVolumeCorrection(true);
+
+    ThermodynamicOperations testOps = new ThermodynamicOperations(testSystem);
+    testOps.TPflash();
+    testSystem.initPhysicalProperties();
+
+    Assertions.assertEquals(testSystem.getPhase("oil").getFlowRate("m3/hr"),
+        testSystem.getFlowRate("m3/hr"), 1e-4);
+  }
+
 }
