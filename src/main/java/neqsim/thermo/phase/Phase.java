910ea5ec05eec1282567493ee471d1726030771f
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2023-07-11 18:23:57 +0200

    refact: reorder getFlowRate units

--- src/main/java/neqsim/thermo/phase/Phase.java
+++ src/main/java/neqsim/thermo/phase/Phase.java
@@ -2119,15 +2119,15 @@ abstract class Phase implements PhaseInterface {
       return numberOfMolesInPhase * getMolarMass() * 60.0;
     } else if (flowunit.equals("kg/hr")) {
       return numberOfMolesInPhase * getMolarMass() * 3600.0;
-    } else if (flowunit.equals("m3/hr")) {
-      initPhysicalProperties("density");
-      return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3") * 3600.0;
-    } else if (flowunit.equals("m3/min")) {
-      initPhysicalProperties("density");
-      return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3") * 60.0;
     } else if (flowunit.equals("m3/sec")) {
       initPhysicalProperties("density");
       return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3");
+    } else if (flowunit.equals("m3/min")) {
+      initPhysicalProperties("density");
+      return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3") * 60.0;
+    } else if (flowunit.equals("m3/hr")) {
+      initPhysicalProperties("density");
+      return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3") * 3600.0;
     } else if (flowunit.equals("ft3/sec")) {
       initPhysicalProperties("density");
       return numberOfMolesInPhase * getMolarMass() / getDensity("kg/m3") * Math.pow(3.2808399, 3);
