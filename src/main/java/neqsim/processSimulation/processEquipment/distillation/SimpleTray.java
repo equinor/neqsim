c054fb45f1d178575899e80b3209a504556d9ae0
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-07-25 20:54:50 +0000

    added multiphase check

--- src/main/java/neqsim/processSimulation/processEquipment/distillation/SimpleTray.java
+++ src/main/java/neqsim/processSimulation/processEquipment/distillation/SimpleTray.java
@@ -1,6 +1,9 @@
 package neqsim.processSimulation.processEquipment.distillation;
 
 import java.util.UUID;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import neqsim.processSimulation.processEquipment.mixer.Mixer;
 import neqsim.processSimulation.processEquipment.stream.Stream;
 import neqsim.processSimulation.processEquipment.stream.StreamInterface;
 import neqsim.thermo.system.SystemInterface;
@@ -17,6 +20,7 @@ import neqsim.thermodynamicOperations.ThermodynamicOperations;
 public class SimpleTray extends neqsim.processSimulation.processEquipment.mixer.Mixer
     implements TrayInterface {
   private static final long serialVersionUID = 1000;
+  static Logger logger = LogManager.getLogger(SimpleTray.class);
 
   double heatInput = 0.0;
   private double temperature = Double.NaN;
@@ -164,6 +168,12 @@ public class SimpleTray extends neqsim.processSimulation.processEquipment.mixer.
     setTemperature(mixedStream.getTemperature());
     mixedStream.setCalculationIdentifier(id);
     setCalculationIdentifier(id);
+
+    if (mixedStream.getFluid().getNumberOfPhases() >= 3) {
+      System.out
+          .println("error...." + mixedStream.getFluid().getNumberOfPhases() + "phases on tray");
+      logger.warn("error...." + mixedStream.getFluid().getNumberOfPhases() + "phases on tray");
+    }
   }
 
   /**
