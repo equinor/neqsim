3ca8babb8aa9fc9b77cda01cd65b9d944d9d6742
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-07-28 07:06:45 +0000

    added pressure drop element

---
+++ src/main/java/neqsim/processSimulation/processEquipment/util/PressureDrop.java
@@ -0,0 +1,74 @@
+package neqsim.processSimulation.processEquipment.util;
+
+import java.util.UUID;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import neqsim.processSimulation.processEquipment.stream.StreamInterface;
+import neqsim.processSimulation.processEquipment.valve.ThrottlingValve;
+import neqsim.thermo.system.SystemInterface;
+import neqsim.thermodynamicOperations.ThermodynamicOperations;
+
+/**
+ * <p>
+ * PressureDrop class.
+ * </p>
+ * The pressure drop unit is used to simulate pressure drops in process plant. The proessure drop is
+ * simulated using a constant enthalpy flash.
+ * 
+ * @author esol
+ * @version $Id: $Id
+ */
+public class PressureDrop extends ThrottlingValve {
+  private static final long serialVersionUID = 1000;
+  static Logger logger = LogManager.getLogger(PressureDrop.class);
+
+  SystemInterface thermoSystem;
+  double pressureDrop = 0.1;
+
+  /**
+   * <p>
+   * Constructor for PressureDrop.
+   * </p>
+   */
+  public PressureDrop(String name) {
+    super(name);
+  }
+
+  public void setPressureDrop(double pressureDrop, String unit) {
+    if (unit.equals("bara")) {
+      this.pressureDrop = pressureDrop;
+    }
+    if (unit.equals("Pa")) {
+      this.pressureDrop = pressureDrop / 1e5;
+    }
+  }
+
+  /**
+   * <p>
+   * Constructor for PressureDropUnit.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @param inletStream a {@link neqsim.processSimulation.processEquipment.stream.StreamInterface}
+   *        object
+   */
+  public PressureDrop(String name, StreamInterface inletStream) {
+    this(name);
+    setInletStream(inletStream);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void run(UUID id) {
+    thermoSystem = getInletStream().getThermoSystem().clone();
+    ThermodynamicOperations thermoOps = new ThermodynamicOperations(thermoSystem);
+    thermoSystem.init(3);
+    double enthalpy = thermoSystem.getEnthalpy();
+
+    thermoSystem.setPressure(thermoSystem.getPressure() - pressureDrop);
+    thermoOps.PHflash(enthalpy);
+
+    outStream.setFluid(thermoSystem);
+  }
+
+}
