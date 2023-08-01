79edc56cd5ac8fe5e51af877aac6ce24de870c10
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-08-01 07:37:31 +0000

    add barg as unit in setPressure

--- src/main/java/neqsim/util/unit/PressureUnit.java
+++ src/main/java/neqsim/util/unit/PressureUnit.java
@@ -1,5 +1,7 @@
 package neqsim.util.unit;
 
+import neqsim.util.exception.InvalidInputException;
+
 /**
  * <p>
  * PressureUnit class.
@@ -9,61 +11,72 @@ package neqsim.util.unit;
  * @version $Id: $Id
  */
 public class PressureUnit extends neqsim.util.unit.BaseUnit {
-    private static final long serialVersionUID = 1000;
+  private static final long serialVersionUID = 1000;
 
-    /**
-     * <p>
-     * Constructor for PressureUnit.
-     * </p>
-     *
-     * @param value a double
-     * @param name a {@link java.lang.String} object
-     */
-    public PressureUnit(double value, String name) {
-        super(value, name);
-    }
+  /**
+   * <p>
+   * Constructor for PressureUnit.
+   * </p>
+   *
+   * @param value a double
+   * @param name a {@link java.lang.String} object
+   */
+  public PressureUnit(double value, String name) {
+    super(value, name);
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getValue(double val, String fromunit, String tounit) {
-        invalue = val;
-        return getConversionFactor(fromunit) / getConversionFactor(tounit) * invalue;
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getValue(double val, String fromunit, String tounit) {
+    invalue = val;
+    return getConversionFactor(fromunit) / getConversionFactor(tounit) * invalue;
+  }
 
-    /** {@inheritDoc} */
-    @Override
-    public double getValue(String tounit) {
-        if (tounit.equals("barg")) {
-            return (getConversionFactor(inunit) / getConversionFactor("bara")) * invalue - 1.01325;
-        } else {
-            return getConversionFactor(inunit) / getConversionFactor(tounit) * invalue;
-        }
-    }
+  /** {@inheritDoc} */
+  @Override
+  public double getValue(String tounit) {
+    if (tounit.equals("barg")) {
+      return (getConversionFactor(inunit) / getConversionFactor("bara")) * invalue - 1.01325;
 
-    /**
-     * <p>
-     * getConversionFactor.
-     * </p>
-     *
-     * @param name a {@link java.lang.String} object
-     * @return a double
-     */
-    public double getConversionFactor(String name) {
-        double conversionFactor = 1.0;
-        switch (name) {
-            case "bara":
-                conversionFactor = 1.0;
-                break;
-            case "psi":
-                conversionFactor = 0.06894757;
-                break;
-            case "Pa":
-                conversionFactor = 1.0e-5;
-                break;
-            case "MPa":
-                conversionFactor = 10.0;
-                break;
-        }
-        return conversionFactor;
     }
+    if (inunit.equals("barg")) {
+      return (getConversionFactor(inunit) / getConversionFactor("bara")) * invalue + 1.01325;
+
+    } else {
+      return getConversionFactor(inunit) / getConversionFactor(tounit) * invalue;
+    }
+  }
+
+  /**
+   * <p>
+   * getConversionFactor.
+   * </p>
+   *
+   * @param name a {@link java.lang.String} object
+   * @return a double
+   */
+  public double getConversionFactor(String name) {
+    double conversionFactor = 1.0;
+    switch (name) {
+      case "bara":
+        conversionFactor = 1.0;
+        break;
+      case "barg":
+        conversionFactor = 1.0;
+        break;
+      case "psi":
+        conversionFactor = 0.06894757;
+        break;
+      case "Pa":
+        conversionFactor = 1.0e-5;
+        break;
+      case "MPa":
+        conversionFactor = 10.0;
+        break;
+      default:
+        throw new RuntimeException(
+            new InvalidInputException(this, "getConversionFactor", name, "unit not supproted"));
+    }
+    return conversionFactor;
+  }
 }
