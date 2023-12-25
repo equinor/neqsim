c000018ff31c366b77c867402adb83320fcd0629
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2023-12-25 08:19:43 +0000

    updates

--- src/test/java/neqsim/processSimulation/processEquipment/reservoir/WellFlowTest.java
+++ src/test/java/neqsim/processSimulation/processEquipment/reservoir/WellFlowTest.java
@@ -1,7 +1,9 @@
 package neqsim.processSimulation.processEquipment.reservoir;
 
 import org.junit.jupiter.api.Test;
+import neqsim.processSimulation.processEquipment.pipeline.PipeBeggsAndBrills;
 import neqsim.processSimulation.processEquipment.stream.StreamInterface;
+import neqsim.processSimulation.processEquipment.util.Adjuster;
 import neqsim.processSimulation.processSystem.ProcessSystem;
 
 public class WellFlowTest {
@@ -39,20 +41,80 @@ public class WellFlowTest {
     System.out
         .println("pres bottomhole " + wellflow.getOutletStream().getPressure("bara") + " bara");
 
-    process.setTimeStep(60 * 60 * 24 * 365);
-
-    for (int i = 0; i < 3; i++) {
-      process.runTransient();
-      System.out.println("reservoir pressure " + wellflow.getInletStream().getPressure("bara"));
-      System.out
-          .println("pres bottomhole " + wellflow.getOutletStream().getPressure("bara") + " bara");
-    }
-
   }
 
   @Test
   void testRunTransient() {
+    neqsim.thermo.system.SystemInterface fluid1 =
+        new neqsim.thermo.system.SystemPrEos(373.15, 100.0);
+    fluid1.addComponent("water", 3.599);
+    fluid1.addComponent("nitrogen", 0.599);
+    fluid1.addComponent("CO2", 0.51);
+    fluid1.addComponent("methane", 62.8);
+    fluid1.addComponent("n-heptane", 12.8);
+    fluid1.setMixingRule(2);
+    fluid1.setMultiPhaseCheck(true);
 
+    SimpleReservoir reservoirOps = new SimpleReservoir("Well 1 reservoir");
+    reservoirOps.setReservoirFluid(fluid1, 1e9, 10.0, 10.0e7);
+
+    StreamInterface producedGasStream = reservoirOps.addGasProducer("gasproducer_1");
+    producedGasStream.setFlowRate(1.0, "MSm3/day");
+
+    WellFlow wellflow = new WellFlow("well flow unit");
+    wellflow.setInletStream(producedGasStream);
+    wellflow.setWellProductionIndex(5.000100751427403E-4);
+
+    PipeBeggsAndBrills pipe = new PipeBeggsAndBrills(wellflow.getOutletStream());
+    pipe.setPipeWallRoughness(5e-6);
+    pipe.setLength(300.0);
+    pipe.setElevation(100);
+    pipe.setDiameter(0.325);
+
+    PipeBeggsAndBrills pipeline = new PipeBeggsAndBrills(wellflow.getOutletStream());
+    pipeline.setPipeWallRoughness(5e-6);
+    pipeline.setLength(60000.0);
+    pipeline.setElevation(200);
+    pipeline.setDiameter(0.325);
+
+    Adjuster adjuster = new Adjuster("adjuster");
+    adjuster.setTargetVariable(pipeline.getOutletStream(), "pressure", 80.0, "bara");
+    adjuster.setAdjustedVariable(producedGasStream, "molarFlow");
+
+
+    ProcessSystem process = new ProcessSystem();
+    process.add(reservoirOps);
+    process.add(wellflow);
+    process.add(pipe);
+    process.add(pipeline);
+    process.add(adjuster);
+    process.run();
+
+    System.out.println("production flow rate " + producedGasStream.getFlowRate("MSm3/day"));
+
+    System.out.println("production index " + wellflow.getWellProductionIndex() + " MSm3/day/bar^2");
+    System.out.println("reservoir pressure " + producedGasStream.getPressure("bara"));
+    System.out
+        .println("pres bottomhole " + wellflow.getOutletStream().getPressure("bara") + " bara");
+    System.out.println("xmas pressure " + pipe.getOutletStream().getPressure("bara") + " bara");
+    System.out
+        .println("top side pressure " + pipeline.getOutletStream().getPressure("bara") + " bara");
+
+    // process.setTimeStep(60 * 60 * 24 * 365);
+
+    for (int i = 0; i < 5; i++) {
+      reservoirOps.runTransient(60 * 60 * 24 * 365);
+      process.run();
+      System.out.println("production flow rate " + producedGasStream.getFlowRate("MSm3/day"));
+      System.out.println("reservoir pressure " + wellflow.getInletStream().getPressure("bara"));
+      System.out
+          .println("pres bottomhole " + wellflow.getOutletStream().getPressure("bara") + " bara");
+
+      System.out.println("xmas pressure " + pipe.getOutletStream().getPressure("bara") + " bara");
+      System.out
+          .println("top side pressure " + pipeline.getOutletStream().getPressure("bara") + " bara");
+
+    }
   }
 
 
