f56095db5dc32aecb1301ef443872dba9842b349
Sviatoslav Eroshkin <109044598+Sviatose@users.noreply.github.com>
2024-05-06 13:41:21 +0000

    v1

---
+++ src/main/java/neqsim/thermo/system/SystemSrkCPASalt.java
@@ -0,0 +1,80 @@
+package neqsim.thermo.system;
+
+import neqsim.thermo.phase.PhaseHydrate;
+import neqsim.thermo.phase.PhasePureComponentSolid;
+import neqsim.thermo.phase.PhaseSrkCPAs;
+
+/**
+ * This class defines a thermodynamic system using the CPA-EOS of Equinor equation of state.
+ *
+ * @author Even Solbraa
+ * @version $Id: $Id
+ */
+public class SystemSrkCPASalt extends SystemSrkCPAs {
+  private static final long serialVersionUID = 1000;
+
+  /**
+   * Constructor of a fluid object using the CPA-EoS version of Equinor.
+   */
+  public SystemSrkCPASalt() {
+    this(298.15, 1.0, false);
+  }
+
+  /**
+   * Constructor of a fluid object using the CPA-EoS version of Equinor.
+   *
+   * @param T The temperature in unit Kelvin
+   * @param P The pressure in unit bara (absolute pressure)
+   */
+  public SystemSrkCPASalt(double T, double P) {
+    this(T, P, false);
+  }
+
+  /**
+   * Constructor of a fluid object using the CPA-EoS version of Equinor.
+   *
+   * @param T The temperature in unit Kelvin
+   * @param P The pressure in unit bara (absolute pressure)
+   * @param checkForSolids Set true to do solid phase check and calculations
+   */
+  public SystemSrkCPASalt(double T, double P, boolean checkForSolids) {
+    super(T, P, checkForSolids);
+    modelName = "CPAs-SRK-EOS-statoil";
+    attractiveTermNumber = 15;
+
+    // Recreates phases created in super constructor SystemSrkCPAs
+    for (int i = 0; i < numberOfPhases; i++) {
+      phaseArray[i] = new PhaseSrkCPAs();
+      phaseArray[i].setTemperature(T);
+      phaseArray[i].setPressure(P);
+    }
+    this.useVolumeCorrection(true);
+
+    if (solidPhaseCheck) {
+      phaseArray[numberOfPhases - 1] = new PhasePureComponentSolid();
+      phaseArray[numberOfPhases - 1].setTemperature(T);
+      phaseArray[numberOfPhases - 1].setPressure(P);
+      phaseArray[numberOfPhases - 1].setRefPhase(phaseArray[1].getRefPhase());
+    }
+
+    if (hydrateCheck) {
+      phaseArray[numberOfPhases - 1] = new PhaseHydrate();
+      phaseArray[numberOfPhases - 1].setTemperature(T);
+      phaseArray[numberOfPhases - 1].setPressure(P);
+      phaseArray[numberOfPhases - 1].setRefPhase(phaseArray[1].getRefPhase());
+    }
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public SystemSrkCPAstatoil clone() {
+    SystemSrkCPAstatoil clonedSystem = null;
+    try {
+      clonedSystem = (SystemSrkCPAstatoil) super.clone();
+    } catch (Exception ex) {
+      logger.error("Cloning failed.", ex);
+    }
+
+    return clonedSystem;
+  }
+}
---
+++ src/test/java/neqsim/thermo/system/SystemSrkCPASaltTest.java
@@ -0,0 +1,94 @@
+package neqsim.thermo.system;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import neqsim.thermodynamicOperations.ThermodynamicOperations;
+
+/**
+ * <p>
+ * CPAEosTest class.
+ * </p>
+ *
+ * @author ESOL
+ * @version $Id: $Id
+ * @since 2.2.3
+ */
+public class SystemSrkCPASaltTest extends neqsim.NeqSimTest{
+    static SystemInterface thermoSystem = null;
+
+    /**
+     * <p>
+     * setUp.
+     * </p>
+     */
+    @BeforeAll
+    public static void setUp() {
+        thermoSystem = new SystemSrkCPASalt(298.0, 10.0);
+        thermoSystem.addComponent("methane", 11.0);
+        thermoSystem.addComponent("CO2", 1.0);
+        thermoSystem.addComponent("MEG", 1.0);
+        thermoSystem.addComponent("water", 11.0);
+        thermoSystem.createDatabase(true);
+        thermoSystem.setMixingRule(10);
+    }
+
+    /**
+     * <p>
+     * testTPflash.
+     * </p>
+     */
+    @Test
+    public void testTPflash() {
+        ThermodynamicOperations testOps = new ThermodynamicOperations(thermoSystem);
+        testOps.TPflash();
+        assertEquals(2, thermoSystem.getNumberOfPhases());
+    }
+
+    /**
+     * <p>
+     * testinitPhysicalProperties.
+     * </p>
+     */
+    @Test
+    public void testinitPhysicalProperties() {
+        thermoSystem.initPhysicalProperties();
+        assertEquals(thermoSystem.getPhase(0).getPhysicalProperties().getDensity(),
+                thermoSystem.getPhase(0).getPhysicalProperties().getDensity());
+                System.out.println("Finished");
+    }
+
+    /**
+     * <p>
+     * testPHflash.
+     * </p>
+     */
+    @Test
+    public void testPHflash() {
+        ThermodynamicOperations testOps = new ThermodynamicOperations(thermoSystem);
+        testOps.TPflash();
+        thermoSystem.init(3);
+        double enthalpy = thermoSystem.getEnthalpy();
+        testOps.PHflash(enthalpy + 10.0);
+        thermoSystem.init(3);
+
+        assertEquals(Math.round(enthalpy + 10.0), Math.round(thermoSystem.getEnthalpy()));
+    }
+
+    /**
+     * <p>
+     * testPSflash.
+     * </p>
+     */
+    @Test
+    public void testPSflash() {
+        ThermodynamicOperations testOps = new ThermodynamicOperations(thermoSystem);
+        testOps.TPflash();
+        thermoSystem.init(3);
+        double entropy = thermoSystem.getEntropy();
+        testOps.PSflash(entropy + 10.0);
+        thermoSystem.init(3);
+
+        assertEquals(Math.round(entropy + 10.0), Math.round(thermoSystem.getEntropy()));
+    }
+}
