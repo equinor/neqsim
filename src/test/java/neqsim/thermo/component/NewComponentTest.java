6017ac15b0c8429bc775faf7445b6112848dec5a
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-07-02 20:34:04 +0000

    add set and get methods and a test

--- src/test/java/neqsim/thermo/component/NewComponentTest.java
+++ src/test/java/neqsim/thermo/component/NewComponentTest.java
@@ -67,4 +67,32 @@ public class NewComponentTest extends neqsim.NeqSimTest {
     thermoSystem.init(0);
     assertEquals(56.10632e-3, thermoSystem.getMolarMass("kg/mol"), 0.01);
   }
+
+  @Test
+  public void setTCPC_test() {
+    thermoSystem = new SystemSrkEos(298.0, ThermodynamicConstantsInterface.referencePressure);
+    thermoSystem.addComponent("iso-butene", 1.0);
+    thermoSystem.init(0);
+    thermoSystem.getPhase(0).getComponent(0).setTC(190.0);
+    assertEquals(190.0, thermoSystem.getPhase(0).getComponent(0).getTC(), 0.01);
+
+    thermoSystem.getPhase(0).getComponent(0).setTC(190.0, "R");
+    assertEquals(190.0, thermoSystem.getPhase(0).getComponent(0).getTC("R"), 0.01);
+
+    thermoSystem.getPhase(0).getComponent(0).setTC(190.0, "R");
+    assertEquals(105.55555, thermoSystem.getPhase(0).getComponent(0).getTC("K"), 0.01);
+
+    thermoSystem.getPhase(0).getComponent(0).setPC(1290.0, "psia");
+    assertEquals(1290.0, thermoSystem.getPhase(0).getComponent(0).getPC("psia"), 0.01);
+    assertEquals(88.9423690, thermoSystem.getPhase(0).getComponent(0).getPC("bara"), 0.01);
+
+    thermoSystem.getPhase(0).getComponent(0).setMolarMass(0.090);
+    assertEquals(0.09, thermoSystem.getPhase(0).getComponent(0).getMolarMass(), 0.01);
+    assertEquals(90.0, thermoSystem.getPhase(0).getComponent(0).getMolarMass("gr/mol"), 0.01);
+    assertEquals(90.0, thermoSystem.getPhase(0).getComponent(0).getMolarMass("lbm/lbmol"), 0.01);
+
+    thermoSystem.getPhase(0).getComponent(0).setMolarMass(85.0, "lbm/lbmol");
+    assertEquals(85.0, thermoSystem.getPhase(0).getComponent(0).getMolarMass("gr/mol"), 0.01);
+
+  }
 }
