e62dee110ea1f958995e8eba1eeef9ba6672ab9d
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-07-31 19:42:44 +0000

    added duty in output

--- src/main/java/neqsim/processSimulation/util/monitor/HXResponse.java
+++ src/main/java/neqsim/processSimulation/util/monitor/HXResponse.java
@@ -21,6 +21,8 @@ public class HXResponse {
   public Double dischargeTemperature2;
 
   public Double dutyBalance;
+  public Double duty;
+  public Double UAvalue;
 
   /**
    * <p>
@@ -37,17 +39,15 @@ public class HXResponse {
    * @param inputHeatExchenger a
    *        {@link neqsim.processSimulation.processEquipment.heatExchanger.HeatExchanger} object
    */
-  public HXResponse(HeatExchanger inputHeatExchenger) {
-    name = inputHeatExchenger.getName();
-
-    feedTemperature1 = inputHeatExchenger.getInStream(0).getTemperature("C");
-    dischargeTemperature1 = inputHeatExchenger.getOutStream(0).getTemperature("C");
-
-    feedTemperature2 = inputHeatExchenger.getInStream(1).getTemperature("C");
-    dischargeTemperature2 = inputHeatExchenger.getOutStream(1).getTemperature("C");
-
-    HXthermalEfectiveness = inputHeatExchenger.getThermalEffectiveness();
-
-    dutyBalance = inputHeatExchenger.getHotColdDutyBalance();
+  public HXResponse(HeatExchanger inputHeatExchanger) {
+    name = inputHeatExchanger.getName();
+    feedTemperature1 = inputHeatExchanger.getInStream(0).getTemperature("C");
+    dischargeTemperature1 = inputHeatExchanger.getOutStream(0).getTemperature("C");
+    feedTemperature2 = inputHeatExchanger.getInStream(1).getTemperature("C");
+    dischargeTemperature2 = inputHeatExchanger.getOutStream(1).getTemperature("C");
+    HXthermalEfectiveness = inputHeatExchanger.getThermalEffectiveness();
+    dutyBalance = inputHeatExchanger.getHotColdDutyBalance();
+    duty = inputHeatExchanger.getDuty();
+    UAvalue = inputHeatExchanger.getUAvalue();
   }
 }
