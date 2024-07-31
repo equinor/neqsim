752e4ec3935d289193da7afd94ac2cb999bcdab0
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-07-31 22:50:32 +0200

    added pump output (#1060)

--- src/test/java/neqsim/processSimulation/util/monitor/SeparatorResponseTest.java
+++ src/test/java/neqsim/processSimulation/util/monitor/SeparatorResponseTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import neqsim.processSimulation.processEquipment.heatExchanger.HeatExchanger;
+import neqsim.processSimulation.processEquipment.pump.Pump;
 import neqsim.processSimulation.processEquipment.separator.Separator;
 import neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator;
 import neqsim.processSimulation.processEquipment.stream.Stream;
@@ -42,23 +43,27 @@ public class SeparatorResponseTest {
     hx1.setName("E-100");
     hx1.setGuessOutTemperature(273.15 + 35.0);
     hx1.setUAvalue(444000.2);
-    hx1.setFeedStream(1, inletStream);
+    hx1.setFeedStream(1, separator3phase.getOilOutStream());
+
+    Pump pump1 = new Pump(separator3phase.getOilOutStream());
+    pump1.setOutletPressure(100.0);
 
     processOps.add(inletStream);
     processOps.add(separator);
     processOps.add(separator3phase);
-    processOps.add(hx1);
+    // processOps.add(hx1);
+    processOps.add(pump1);
 
     processOps.run();
 
     String sepjson = separator.toJson();
     String sep3json = separator3phase.toJson();
     String hxjson = hx1.toJson();
+    String pumpjson = pump1.toJson();
     JsonObject jsonObject = JsonParser.parseString(sep3json).getAsJsonObject();
     Double reldens = jsonObject.getAsJsonObject("feed").getAsJsonObject("properties")
         .getAsJsonObject("oil").getAsJsonObject("relative density").get("value").getAsDouble();
     assertEquals(0.688292615281, reldens, 0.01);
-
 
   }
 }
