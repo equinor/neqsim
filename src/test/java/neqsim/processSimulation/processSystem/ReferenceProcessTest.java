f7020671d9f830cacbcecd965fec0e538960ae70
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-09-07 05:43:15 +0000

    added test

--- src/test/java/neqsim/processSimulation/processSystem/ReferenceProcessTest.java
+++ src/test/java/neqsim/processSimulation/processSystem/ReferenceProcessTest.java
@@ -1,15 +1,14 @@
 package neqsim.processSimulation.processSystem;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import neqsim.thermo.system.SystemInterface;
+import neqsim.thermo.system.SystemSrkEos;
 
 public class ReferenceProcessTest extends neqsim.NeqSimTest {
 
   @Test
   public void testProcess() {
     SystemInterface thermoSystem = new SystemSrkEos(298.0, 10.0);
-    thermoSystem.addComponent("water", 51.0);
     thermoSystem.addComponent("nitrogen", 51.0);
     thermoSystem.addComponent("CO2", 51.0);
     thermoSystem.addComponent("methane", 51.0);
@@ -35,112 +34,87 @@ public class ReferenceProcessTest extends neqsim.NeqSimTest {
     thermoSystem.addPlusFraction("C38_C80", 1.0, 662.0 / 1000.0, 0.92);
     thermoSystem.setMixingRule("classic");
     thermoSystem.setMultiPhaseCheck(true);
-    thermoSystem.setMolarComposition(new double[] {0.034266, 0.005269, 0.039189, 0.700553, 0.091154,
+    thermoSystem.setMolarComposition(new double[] {0.005269, 0.039189, 0.700553, 0.091154,
         0.050908, 0.007751, 0.014665, 0.004249, 0.004878, 0.004541, 0.007189, 0.006904, 0.004355,
         0.007658, 0.003861, 0.003301, 0.002624, 0.001857, 0.001320, 0.001426, 0.001164, 0.000916});
 
-    Stream feedStream = new Stream("feed stream", thermoSystem);
-    feedStream.setFlowRate(604094, "kg/hr");
-    feedStream.setTemperature(25.5, "C");
-    feedStream.setPressure(26.0, "bara");
+    neqsim.processSimulation.processEquipment.stream.Stream wellStream =
+        new neqsim.processSimulation.processEquipment.stream.Stream(thermoSystem);
+    wellStream.setName("well stream");
+    wellStream.setTemperature(70.0, "C");
+    wellStream.setPressure(90.0, "bara");
 
-    Separator inletSeparator = new Separator("inlet separator", feedStream);
+    neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator firstStageSeparator =
+        new neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator(wellStream);
+    firstStageSeparator.setName("1st stage separator");
 
-    Heater oilHeater = new Heater("oil heater", inletSeparator.getLiquidOutStream());
-    oilHeater.setOutTemperature(60.0, "C");
+    neqsim.processSimulation.processEquipment.heatExchanger.Heater oilHeaterSecondStage =
+        new neqsim.processSimulation.processEquipment.heatExchanger.Heater(
+            firstStageSeparator.getOilOutStream());
+    oilHeaterSecondStage.setName("oil heater second stage");
+    oilHeaterSecondStage.setOutPressure(30.0);
+    oilHeaterSecondStage.setOutTemperature(68.0, "C");
 
-    ThrottlingValve valve1 = new ThrottlingValve("valve oil", oilHeater.getOutletStream());
-    valve1.setOutletPressure(10.0, "bara");
+    neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator secondStageSeparator =
+        new neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator(
+            oilHeaterSecondStage.getOutStream());
+    secondStageSeparator.setName("2nd stage separator");
 
-    StreamInterface resycleScrubberStream = feedStream.clone();
-    resycleScrubberStream.setFlowRate(1.0, "kg/hr");
+    neqsim.processSimulation.processEquipment.heatExchanger.Heater oilHeaterThirdStage =
+        new neqsim.processSimulation.processEquipment.heatExchanger.Heater(
+            secondStageSeparator.getOilOutStream());
+    oilHeaterThirdStage.setName("oil heater third stage");
+    oilHeaterThirdStage.setOutPressure(2.8);
+    oilHeaterThirdStage.setOutTemperature(70.0, "C");
 
-    ThreePhaseSeparator secondStageSeparator =
-        new ThreePhaseSeparator("inlet separator", valve1.getOutletStream());
-    secondStageSeparator.addStream(resycleScrubberStream);
+    neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator thirdStageSeparator =
+        new neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator(
+            oilHeaterThirdStage.getOutStream());
+    thirdStageSeparator.setName("3rd stage separator");
 
-    StreamInterface gasResycleStream = feedStream.clone();
-    gasResycleStream.setFlowRate(1.0, "kg/hr");
+    neqsim.processSimulation.processEquipment.heatExchanger.Heater oilHeaterStandardStage =
+        new neqsim.processSimulation.processEquipment.heatExchanger.Heater(
+            thirdStageSeparator.getOilOutStream());
+    oilHeaterStandardStage.setName("oil heater standard stage");
+    oilHeaterStandardStage.setOutPressure(1.01325);
+    oilHeaterStandardStage.setOutTemperature(15.0, "C");
 
-    Mixer gasmixer = new Mixer("gas recycle mixer");
-    gasmixer.addStream(secondStageSeparator.getGasOutStream());
-    gasmixer.addStream(gasResycleStream);
+    neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator standardStageSeparator =
+        new neqsim.processSimulation.processEquipment.separator.ThreePhaseSeparator(
+            oilHeaterStandardStage.getOutStream());
+    standardStageSeparator.setName("standard stage separator");
 
-    // Setting up compressor module
-    Compressor seccondStageCompressor =
-        new Compressor("2nd stage compressor", gasmixer.getOutletStream());
-    seccondStageCompressor.setUsePolytropicCalc(true);
-    seccondStageCompressor.setPolytropicEfficiency(0.9);
-    seccondStageCompressor.setOutletPressure(26.0, "bara");
+    neqsim.processSimulation.processEquipment.mixer.Mixer gasMixer =
+        new neqsim.processSimulation.processEquipment.mixer.Mixer("gas mixer");
+    gasMixer.addStream(firstStageSeparator.getGasOutStream());
+    gasMixer.addStream(secondStageSeparator.getGasOutStream());
+    gasMixer.addStream(thirdStageSeparator.getGasOutStream());
+    gasMixer.addStream(standardStageSeparator.getGasOutStream());
 
-    Cooler afterCooler =
-        new Cooler("2nd stage after cooler", seccondStageCompressor.getOutletStream());
-    afterCooler.setOutTemperature(25.0, "C");
+    neqsim.processSimulation.processEquipment.stream.Stream exportoil =
+        new neqsim.processSimulation.processEquipment.stream.Stream("export oil",
+            standardStageSeparator.getOilOutStream());
+    neqsim.processSimulation.processEquipment.stream.Stream exportgas =
+        new neqsim.processSimulation.processEquipment.stream.Stream("export gas",
+            gasMixer.getOutStream());
 
-    Separator scrubber1 = new Separator("after cooler scrubber", afterCooler.getOutletStream());
-
-    Stream gasFromScrubber = new Stream("gas from scrubber", scrubber1.getGasOutStream());
-
-    Splitter gassplitter = new Splitter("gas splitter", gasFromScrubber);
-    gassplitter.setSplitFactors(new double[] {0.1, 0.9});
-
-    ThrottlingValve recycleValve =
-        new ThrottlingValve("antisurge valve", gassplitter.getSplitStream(0));
-    recycleValve.setOutletPressure(10.0, "bara");
-
-    Recycle recycle2 = new Recycle("recycle 2");
-    recycle2.addStream(recycleValve.getOutletStream());
-    recycle2.setOutletStream(gasResycleStream);
-
-    Recycle recycle1 = new Recycle("recycle 1");
-    recycle1.addStream(scrubber1.getLiquidOutStream());
-    recycle1.setOutletStream(resycleScrubberStream);
-
-    neqsim.processSimulation.processSystem.ProcessSystem operations =
+    neqsim.processSimulation.processSystem.ProcessSystem oilprocess =
         new neqsim.processSimulation.processSystem.ProcessSystem();
-    operations.add(feedStream);
-    operations.add(inletSeparator);
-    operations.add(oilHeater);
-    operations.add(valve1);
-    operations.add(resycleScrubberStream);
-    operations.add(secondStageSeparator);
-    operations.add(gasResycleStream);
-    operations.add(gasmixer);
-    operations.add(seccondStageCompressor);
-    operations.add(afterCooler);
-    operations.add(scrubber1);
-    operations.add(gasFromScrubber);
-    operations.add(gassplitter);
-    operations.add(recycleValve);
-    operations.add(recycle2);
-    operations.add(recycle1);
+    oilprocess.add(wellStream);
+    oilprocess.add(firstStageSeparator);
+    oilprocess.add(oilHeaterSecondStage);
+    oilprocess.add(secondStageSeparator);
+    oilprocess.add(oilHeaterThirdStage);
+    oilprocess.add(thirdStageSeparator);
+    oilprocess.add(oilHeaterStandardStage);
+    oilprocess.add(standardStageSeparator);
+    oilprocess.add(gasMixer);
+    oilprocess.add(exportoil);
+    oilprocess.add(exportgas);
 
-    operations.run();
+    oilprocess.run();
 
-    assertEquals(2046.8012652616517
-    , resycleScrubberStream.getFlowRate("kg/hr"), 0.1);
-
-    neqsim.processSimulation.processEquipment.compressor.CompressorChartGenerator compchartgenerator =
-        new neqsim.processSimulation.processEquipment.compressor.CompressorChartGenerator(
-            seccondStageCompressor);
-    compchartgenerator.generateCompressorChart("mid range");
-
-    seccondStageCompressor.setCompressorChart(compchartgenerator.generateCompressorChart("normal"));
-    seccondStageCompressor.getCompressorChart().setUseCompressorChart(true);
-    operations.run();
-
-    assertEquals(25.86689, seccondStageCompressor.getOutletStream().getPressure("bara"), 0.5);
-
-    seccondStageCompressor.setSpeed(seccondStageCompressor.getSpeed() + 500);
-    operations.run();
-
-    assertEquals(40.5019771, seccondStageCompressor.getOutletStream().getPressure("bara"), 0.5);
-
-    feedStream.setFlowRate(204094, "kg/hr");
-    operations.run();
-
-    assertTrue(seccondStageCompressor.isSurge(seccondStageCompressor.getPolytropicFluidHead(),
-        seccondStageCompressor.getInletStream().getFlowRate("m3/hr")));
+    standardStageSeparator.run();
 
   }
 
