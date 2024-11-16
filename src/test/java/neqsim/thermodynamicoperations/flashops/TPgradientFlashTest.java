95e94cf9c968aac43752de001eece885973d0117
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-11-16 09:05:56 +0000

    update

--- src/test/java/neqsim/thermodynamicoperations/flashops/TPgradientFlashTest.java
+++ src/test/java/neqsim/thermodynamicoperations/flashops/TPgradientFlashTest.java
@@ -1,6 +1,7 @@
 package neqsim.thermodynamicoperations.flashops;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.ArrayList;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
@@ -25,17 +26,50 @@ public class TPgradientFlashTest {
 
     SystemInterface newSystem = null;
     try {
-      try {
-        newSystem = testOps.TPgradientFlash(1000, 355).phaseToSystem(0);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+      newSystem = testOps.TPgradientFlash(1000, 355).phaseToSystem(0);
       // newSystem.prettyPrint();
     } catch (Exception ex) {
       logger.error(ex.getMessage(), ex);
     }
 
     assertEquals(0.095513700959, newSystem.getComponent("hydrogen").getx(), 1e-4);
+  }
+
+  @Test
+  void testGradient() {
+
+    double depth = 1000.0;
+    double temperature = 273.15 + 70;
+    double pressure = 80.0;
+
+    ArrayList<Double> x_h2 = new ArrayList<>();
+    ArrayList<Double> p_depth = new ArrayList<>();
+
+    SystemInterface testSystem = new SystemSrkEos(temperature, pressure);
+    testSystem.addComponent("hydrogen", 1.0);
+    testSystem.addComponent("methane", 8.0);
+    testSystem.addComponent("propane", 1.0);
+
+    testSystem.createDatabase(true);
+    testSystem.setMixingRule(2);
+
+    ThermodynamicOperations testOps = new ThermodynamicOperations(testSystem);
+    SystemInterface newSystem = null;
+    double deltaHeight = 100.0;
+    double deltaT = 0.5;
+    for (int i = 0; i < 10; i++) {
+      try {
+        newSystem = testOps.TPgradientFlash(i * deltaHeight, temperature + i * deltaT);
+      } catch (Exception ex) {
+        logger.error(ex.getMessage(), ex);
+      }
+      x_h2.add(newSystem.getComponent("hydrogen").getx());
+      p_depth.add(newSystem.getPressure());
+      // System.out
+      // .println(newSystem.getComponent("hydrogen").getx() + " " + newSystem.getPressure());
+    }
+    assertEquals(0.0964169380341, x_h2.get(6), 1e-4);
+
   }
 
 }
