6b271dd2414099f4c7a7de6c9cf009c3d4869c27
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2024-12-08 18:54:30 +0000

    update Rachford rice method

--- src/main/java/neqsim/thermodynamicoperations/flashops/TPflash.java
+++ src/main/java/neqsim/thermodynamicoperations/flashops/TPflash.java
@@ -93,8 +93,8 @@ public class TPflash extends Flash {
 
     RachfordRice rachfordRice = new RachfordRice();
     try {
-      system.setBeta(rachfordRice.calcBetaS(system));
-      // system.setBeta(rachfordRice.calcBeta(system.getKvector(), system.getzvector()));
+      // system.setBeta(rachfordRice.calcBetaS(system));
+      system.setBeta(rachfordRice.calcBeta(system.getKvector(), system.getzvector()));
     } catch (IsNaNException ex) {
       logger.warn("Not able to calculate beta. Value is NaN");
       system.setBeta(oldBeta);
