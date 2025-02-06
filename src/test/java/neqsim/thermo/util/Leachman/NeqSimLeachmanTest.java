dc1849bfcab1cfcf598958740976e4e1d2ae55fe
Victor Gusland Ihlebæk <victor.gus.ihle@hotmail.no>
2025-02-06 11:34:13 +0000

    file added

---
+++ src/test/java/neqsim/thermo/util/Leachman/NeqSimLeachmanTest.java
@@ -0,0 +1,15 @@
+package neqsim.thermo.util.Leachman;
+
+import org.junit.jupiter.api.Test;
+
+public class NeqSimLeachmanTest {
+    @Test
+    void testGetDensity() {
+
+    }
+
+    @Test
+    void testGetProperties() {
+
+    }
+}
