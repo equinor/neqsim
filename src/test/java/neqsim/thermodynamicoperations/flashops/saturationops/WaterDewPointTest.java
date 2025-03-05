7a2b32b6560af2561e4c24c8d5f7dcfe982a17a1
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-03-05 22:21:31 +0000

    added water dew point checks

--- src/test/java/neqsim/thermodynamicoperations/flashops/saturationops/WaterDewPointTest.java
+++ src/test/java/neqsim/thermodynamicoperations/flashops/saturationops/WaterDewPointTest.java
@@ -6,6 +6,7 @@ import neqsim.process.equipment.stream.Stream;
 import neqsim.process.measurementdevice.WaterDewPointAnalyser;
 import neqsim.thermo.system.SystemGERGwaterEos;
 import neqsim.thermo.system.SystemInterface;
+import neqsim.thermo.system.SystemPrEos;
 import neqsim.thermo.system.SystemSrkCPAstatoil;
 import neqsim.thermo.system.SystemSrkEos;
 import neqsim.thermodynamicoperations.ThermodynamicOperations;
@@ -59,6 +60,39 @@ public class WaterDewPointTest {
     testSystem.addComponent("n-butane", 0.005);
     testSystem.addComponent("water", ppmWater * 1e-6);
     testSystem.setMixingRule(10);
+    testSystem.setMultiPhaseCheck(true);
+
+    // Perform the water dew point calculation
+    ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
+    try {
+      ops.waterDewPointTemperatureMultiphaseFlash();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+    // Get the calculated dew point temperature
+    double dewPointTemperature = testSystem.getTemperature("C");
+
+    // Assert the expected value (this value should be adjusted based on expected results)
+    assertEquals(-24.71094269, dewPointTemperature, 0.1,
+        "Dew point temperature is not as expected");
+  }
+
+  @Test
+  void testWaterDewPointCalculationCPAhydrate() {
+    // Create a new thermodynamic system
+    double ppmWater = 22.0;
+
+    SystemInterface testSystem = new SystemSrkCPAstatoil(260.15, 70.0);
+    testSystem.addComponent("CO2", 0.02);
+    testSystem.addComponent("nitrogen", 0.01);
+    testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
+    testSystem.addComponent("ethane", 0.05);
+    testSystem.addComponent("propane", 0.01);
+    testSystem.addComponent("i-butane", 0.005);
+    testSystem.addComponent("n-butane", 0.005);
+    testSystem.addComponent("water", ppmWater * 1e-6);
+    testSystem.setMixingRule(10);
     testSystem.setHydrateCheck(true);
 
     // Perform the water dew point calculation
@@ -73,7 +107,8 @@ public class WaterDewPointTest {
     double dewPointTemperature = testSystem.getTemperature("C");
 
     // Assert the expected value (this value should be adjusted based on expected results)
-    assertEquals(-12.4926465, dewPointTemperature, 0.1, "Dew point temperature is not as expected");
+    assertEquals(-17.6585425817, dewPointTemperature, 0.1,
+        "Dew point temperature is not as expected");
   }
 
   @Test
@@ -91,7 +126,6 @@ public class WaterDewPointTest {
     testSystem.addComponent("n-butane", 0.005);
     testSystem.addComponent("water", ppmWater * 1e-6);
     testSystem.setMixingRule(8);
-    testSystem.setHydrateCheck(true);
 
     // Perform the water dew point calculation
     ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
@@ -113,7 +147,7 @@ public class WaterDewPointTest {
     // Create a new thermodynamic system
     double ppmWater = 22.0;
 
-    SystemInterface testSystem = new SystemGERGwaterEos(260.15, 70.0);
+    SystemInterface testSystem = new SystemPrEos(260.15, 70.0);
     testSystem.addComponent("CO2", 0.02);
     testSystem.addComponent("nitrogen", 0.01);
     testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
@@ -122,8 +156,7 @@ public class WaterDewPointTest {
     testSystem.addComponent("i-butane", 0.005);
     testSystem.addComponent("n-butane", 0.005);
     testSystem.addComponent("water", ppmWater * 1e-6);
-    testSystem.setMixingRule(8);
-    testSystem.setHydrateCheck(true);
+    testSystem.setMixingRule(2);
 
     Stream stream = new Stream("testStream", testSystem);
     stream.run();
@@ -135,6 +168,56 @@ public class WaterDewPointTest {
     double dewPointTemperature = analyser.getMeasuredValue("C");
 
     // Assert the expected value (this value should be adjusted based on expected results)
-    assertEquals(-10.57625, dewPointTemperature, 0.1, "Dew point temperature is not as expected");
+    assertEquals(-15.307572714, dewPointTemperature, 0.1,
+        "Dew point temperature is not as expected");
+
+    analyser.setMethod("CPA");
+    // Get the calculated dew point temperature
+    dewPointTemperature = analyser.getMeasuredValue("C");
+
+    // Assert the expected value (this value should be adjusted based on expected results)
+    assertEquals(-24.7109426, dewPointTemperature, 0.1, "Dew point temperature is not as expected");
+
+    analyser.setMethod("CPA hydrate");
+    // Get the calculated dew point temperature
+    dewPointTemperature = analyser.getMeasuredValue("C");
+
+    // Assert the expected value (this value should be adjusted based on expected results)
+    assertEquals(-17.658542, dewPointTemperature, 0.1, "Dew point temperature is not as expected");
+
+    analyser.setMethod("Bukacek");
+    // Get the calculated dew point temperature
+    dewPointTemperature = analyser.getMeasuredValue("C");
+
+    // Assert the expected value (this value should be adjusted based on expected results)
+    assertEquals(-23.5090125, dewPointTemperature, 0.1, "Dew point temperature is not as expected");
+
+  }
+
+  @Test
+  void testSaturation() {
+    double ppmWater = 22.0;
+
+    SystemInterface testSystem = new SystemSrkCPAstatoil(273.15 - 24.710, 70.0);
+    testSystem.addComponent("CO2", 0.02);
+    testSystem.addComponent("nitrogen", 0.01);
+    testSystem.addComponent("methane", 0.9 - ppmWater * 1e-6);
+    testSystem.addComponent("ethane", 0.05);
+    testSystem.addComponent("propane", 0.01);
+    testSystem.addComponent("i-butane", 0.005);
+    testSystem.addComponent("n-butane", 0.005);
+    testSystem.addComponent("water", ppmWater * 1e-6);
+    testSystem.setMixingRule(10);
+
+    // Perform the water dew point calculation
+    ThermodynamicOperations ops = new ThermodynamicOperations(testSystem);
+    try {
+      ops.saturateWithWater();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    double waterContent = testSystem.getPhase("gas").getComponent("water").getx() * 1e6;
+    assertEquals(22.0, waterContent, 0.1, "Water content is not as expected");
+
   }
 }
