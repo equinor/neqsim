af1f29205790e5887ffa096cb528bf6ec4726ac2
Victor Gusland Ihlebæk <victor.gus.ihle@hotmail.no>
2025-03-25 20:55:41 +0100

    Vega thermo implement (#1305)
    * add vega thermo classes and test
    
    * further updates
    
    * Fixed bug for one component pipeline
    
    ---------
    
    Co-authored-by: Even Solbraa <41290109+EvenSol@users.noreply.github.com>

--- src/main/java/neqsim/fluidmechanics/flownode/fluidboundary/heatmasstransfercalc/FluidBoundary.java
+++ src/main/java/neqsim/fluidmechanics/flownode/fluidboundary/heatmasstransfercalc/FluidBoundary.java
@@ -83,7 +83,10 @@ public abstract class FluidBoundary implements FluidBoundaryInterface, java.io.S
     jFlux = new Matrix(system.getPhases()[0].getNumberOfComponents() - 1, 1);
     nFlux = new Matrix(system.getPhases()[0].getNumberOfComponents(), 1);
     nFlux.set(0, 0, 0.0);
-    nFlux.set(1, 0, 0.0);
+    if (system.getPhases()[0].getNumberOfComponents() > 1) {
+      nFlux.set(1, 0, 0.0);
+    }
+    //nFlux.set(1, 0, 0.0);
     heatTransferCoefficient = new double[2];
     heatTransferCorrection = new double[2];
     thermodynamicCorrections = new boolean[2];
