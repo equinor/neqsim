0d08d005af2ac5c48989ec569bc4ed6680575ef5
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-06-25 20:19:15 +0000

    change neqsim logging

---
+++ src/test/java/neqsim/util/NeqSimLoggingTest.java
@@ -0,0 +1,32 @@
+package neqsim.util;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+public class NeqSimLoggingTest {
+  // add logger for class
+  private static final Logger logger = LogManager.getLogger(NeqSimLoggingTest.class);
+
+  @Test
+  void testSetGlobalLogging() {
+    // Enable global logging at DEBUG level
+    neqsim.util.NeqSimLogging.setGlobalLogging("DEBUG");
+    logger.info("Testing global logging at DEBUG level...");
+
+    assertEquals("DEBUG", logger.getLevel().toString(),
+        "neqsim logger should be set to DEBUG level.");
+
+    neqsim.util.NeqSimLogging.setGlobalLogging("INFO");
+    logger.info("This logging should not be seen.....Testing global logging at INFO level...");
+
+    // Verify that the neqsim logger is set to OFF
+    assertEquals("INFO", logger.getLevel().toString(), "neqsim logger should be set to OFF.");
+
+    neqsim.util.NeqSimLogging.setGlobalLogging("OFF");
+    assertEquals("OFF", logger.getLevel().toString(), "neqsim logger should be set to OFF.");
+
+
+  }
+}
