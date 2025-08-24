f898168cd30d51876991854a0159b576b70d0d26
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-08-24 10:01:32 +0000

    fix model

--- src/test/java/neqsim/thermo/system/SystemBnsEosParityTest.java
+++ src/test/java/neqsim/thermo/system/SystemBnsEosParityTest.java
@@ -1,7 +1,6 @@
 package neqsim.thermo.system;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import neqsim.thermodynamicoperations.ThermodynamicOperations;
 
@@ -15,7 +14,7 @@ public class SystemBnsEosParityTest {
     sys.setRelativeDensity(0.65);
     sys.setComposition(0.02, 0.0, 0.01, 0.0);
     sys.useVolumeCorrection(true);
-    sys.setMixingRule(2);
+    sys.setMixingRule(12);
     ThermodynamicOperations ops = new ThermodynamicOperations(sys);
     ops.TPflash();
     sys.initPhysicalProperties();
@@ -24,9 +23,9 @@ public class SystemBnsEosParityTest {
     double jt = sys.getPhase(0).getJouleThomsonCoefficient() * 10.0;
     double density = sys.getDensity("kg/m3");
     double gamma = cp / cv;
-    double speed = Math.sqrt(gamma * sys.getPhase(0).getZ()
-        * neqsim.thermo.ThermodynamicConstantsInterface.R * sys.getTemperature()
-        / sys.getMolarMass());
+    double speed =
+        Math.sqrt(gamma * sys.getPhase(0).getZ() * neqsim.thermo.ThermodynamicConstantsInterface.R
+            * sys.getTemperature() / sys.getMolarMass());
     assertEquals(21.832364037459712, cp, 0.01);
     assertEquals(12.821736403643044, cv, 0.01);
     assertEquals(7.8812898778587845, jt, 0.01);
@@ -47,14 +46,16 @@ public class SystemBnsEosParityTest {
     ThermodynamicOperations ops = new ThermodynamicOperations(sys);
     ops.TPflash();
     sys.initPhysicalProperties();
+    double Z = sys.getPhase(0).getZvolcorr();
     double cp = sys.getPhase(0).getCp("J/molK");
     double cv = sys.getPhase(0).getCv("J/molK");
     double jt = sys.getPhase(0).getJouleThomsonCoefficient() * 10.0;
     double density = sys.getDensity("kg/m3");
     double gamma = cp / cv;
-    double speed = Math.sqrt(gamma * sys.getPhase(0).getZ()
-        * neqsim.thermo.ThermodynamicConstantsInterface.R * sys.getTemperature()
-        / sys.getMolarMass());
+    double speed =
+        Math.sqrt(gamma * sys.getPhase(0).getZ() * neqsim.thermo.ThermodynamicConstantsInterface.R
+            * sys.getTemperature() / sys.getMolarMass());
+    assertEquals(0.790903069, Z, 0.01);
     assertEquals(30.504016321027333, cp, 0.01);
     assertEquals(12.821736403643044, cv, 0.01);
     assertEquals(5.09508498857797, jt, 0.01);
