275b94e9ccf678a93d4481a1f843650ab9dbd587
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-08-28 18:18:55 +0000

    update bns

--- src/test/java/neqsim/thermo/system/SystemBnsEosParityTest.java
+++ src/test/java/neqsim/thermo/system/SystemBnsEosParityTest.java
@@ -30,7 +30,7 @@ public class SystemBnsEosParityTest {
     assertEquals(39.11774916142968, cp, 0.1);
     assertEquals(29.815115496874455, cv, 0.1);
     assertEquals(5.456788345314515, jt, 0.2);
-    assertEquals(7.7670262291, density, 0.001);
+    assertEquals(7.750037498921, density, 0.001);
     assertEquals(411.4566767812817, speed, 1.0);
 
 
@@ -59,8 +59,8 @@ public class SystemBnsEosParityTest {
         Math.sqrt(gamma * sys.getPhase(0).getZ() * neqsim.thermo.ThermodynamicConstantsInterface.R
             * sys.getTemperature() / sys.getMolarMass());
 
-    assertEquals(0.7759636493593, Z, 0.01);
-    assertEquals(97.28929633247, density, 0.01);
+    assertEquals(0.790903069244, Z, 0.01);
+    assertEquals(95.451592440, density, 0.01);
     assertEquals(53.7112082181241, cp, 1.0);
     assertEquals(30.73168795781629, cv, 1.0);
     assertEquals(3.800162780643346, jt, 0.1);
@@ -70,17 +70,43 @@ public class SystemBnsEosParityTest {
   }
 
   @Test
+  public void testPythonExample() {
+    SystemBnsEos sys = new SystemBnsEos();
+    sys.setTemperature(48.88889 + 273.15);
+    sys.setPressure(13.78948965 * 10.0);
+    sys.setRelativeDensity(0.8);
+    sys.setComposition(0.2, 0.1, 0.02, 0.1);
+    sys.useVolumeCorrection(true);
+    sys.setMixingRule(12);
+    ThermodynamicOperations ops = new ThermodynamicOperations(sys);
+    try {
+      ops.TPflash();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    sys.initProperties();
+
+    double Z = sys.getZvolcorr();
+    double density = sys.getDensity("kg/m3");
+    double jt = sys.getPhase(0).getJouleThomsonCoefficient() * 10.0;
+
+    assertEquals(0.7941023149604872, Z, 1e-3);
+    assertEquals(150.3029810272768, density, 0.3);
+    assertEquals(3.0063081864897288, jt, 0.2);
+  }
+
+  @Test
   public void testCO2test() {
     SystemBnsEos system = new SystemBnsEos();
     system.setTemperature(60.0, "F");
     system.setPressure(2000.0, "psia");
     system.setAssociatedGas(false);
-    // system.setRelativeDensity(0.65);
+    system.setRelativeDensity(0.65);
     system.setComposition(1.00, 0.0, 0.00, 0.0);
     system.useVolumeCorrection(true);
     system.setMixingRule(12);
     new ThermodynamicOperations(system).TPflash();
     system.initProperties();
-    assertEquals(0.27784922930, system.getZvolcorr(), 1e-7);
+    assertEquals(0.3200133065915, system.getZvolcorr(), 1e-7);
   }
 }
