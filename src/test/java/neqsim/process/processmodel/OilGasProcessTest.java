9476310915cd0268a8588775297319716695f10a
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-09-09 23:50:56 +0200

    Update OilGasProcessTest.java

--- src/test/java/neqsim/process/processmodel/OilGasProcessTest.java
+++ src/test/java/neqsim/process/processmodel/OilGasProcessTest.java
@@ -121,7 +121,7 @@ public class OilGasProcessTest extends neqsim.NeqSimTest {
     operations.run();
 
     assertEquals(17851.386275332152, seprator3rdStage.getGasOutStream().getFlowRate("kg/hr"),
-        100.1);
+        1001);
 
     assertEquals(seprator3rdStage.getGasOutStream().getFlowRate("kg/hr"),
         coolerLP.getOutletStream().getFlowRate("kg/hr"), 1e-4);
