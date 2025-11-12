c884fb5ed884bd91e6995dff31efabbe0e7e9e67
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-11-12 17:23:03 +0100

    update

--- src/main/java/neqsim/thermodynamicoperations/flashops/saturationops/BubblePointPressureFlash.java
+++ src/main/java/neqsim/thermodynamicoperations/flashops/saturationops/BubblePointPressureFlash.java
@@ -152,8 +152,7 @@ public class BubblePointPressureFlash extends ConstantDutyPressureFlash {
         }
         // Add dampening to improve convergence, especially near critical point
         double dampingFactor = 0.7;
-        double newPressure =
-            system.getPressure() * (1.0 - dampingFactor + dampingFactor * ytotal);
+        double newPressure = system.getPressure() * (1.0 - dampingFactor + dampingFactor * ytotal);
         system.setPressure(newPressure);
         // + 0.5*(ytotal*system.getPressure()-system.getPressure()));
         if (system.getPressure() < 0) {
@@ -198,3 +197,5 @@ public class BubblePointPressureFlash extends ConstantDutyPressureFlash {
   @Override
   public void printToFile(String name) {}
 }
+
+
