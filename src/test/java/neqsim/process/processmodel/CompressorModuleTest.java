b78c2c1e18ba4da72c44ddb6ac215c5039e9e9b2
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-11-15 21:22:48 +0100

    Update CompressorModuleTest.java

--- src/test/java/neqsim/process/processmodel/CompressorModuleTest.java
+++ src/test/java/neqsim/process/processmodel/CompressorModuleTest.java
@@ -186,7 +186,7 @@ public class CompressorModuleTest extends neqsim.NeqSimTest {
 
     assertEquals(pressurespeedclac, seccondStageCompressor.getOutletStream().getPressure("bara"),
         0.5);
-    assertEquals(3476.0045401266198, seccondStageCompressor.getSpeed(), 100);
+    assertEquals(3500.0, seccondStageCompressor.getSpeed(), 100);
     assertFalse(seccondStageCompressor.isSurge(seccondStageCompressor.getPolytropicFluidHead(),
         seccondStageCompressor.getInletStream().getFlowRate("m3/hr")));
   }
