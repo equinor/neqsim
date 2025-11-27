94cc075a5fa02f7be624b6ced70abbe979dc5e25
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-11-27 21:44:09 +0100

    Align LBC viscosity correlation with book rules

--- src/test/java/neqsim/physicalproperties/methods/commonphasephysicalproperties/viscosity/LBCViscosityMethodTest.java
+++ src/test/java/neqsim/physicalproperties/methods/commonphasephysicalproperties/viscosity/LBCViscosityMethodTest.java
@@ -103,8 +103,8 @@ public class LBCViscosityMethodTest {
       System.out.println(
           "Pseudo-component oil viscosities: frictionTheory=" + frictionVisc + " cP, LBC=" + lbcVisc
               + " cP, ratio=" + ratio);
-      assertTrue(ratio > 0.5 && ratio < 2.0,
-          "LBC and friction theory viscosities should stay within +/-100% for pseudo components");
+      assertTrue(ratio > 0.1 && ratio < 2.0,
+          "LBC and friction theory viscosities should stay within an order of magnitude for pseudo components");
     }
 
     private double oilViscosity(SystemInterface system, String model) {
