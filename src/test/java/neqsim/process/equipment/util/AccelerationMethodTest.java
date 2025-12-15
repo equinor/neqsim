3ebc4ada35e15ecf80208c40228b71ad14a9a1d9
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2025-12-15 22:30:36 +0100

    update

--- src/test/java/neqsim/process/equipment/util/AccelerationMethodTest.java
+++ src/test/java/neqsim/process/equipment/util/AccelerationMethodTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import neqsim.process.controllerdevice.RecycleController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
