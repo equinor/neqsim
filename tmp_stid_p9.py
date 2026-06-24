dbe6ff28fa518cd0cd2fd6ed2e2ceffc5d3e26a6
Åsmund Våge Fannemel <34712686+asmfstatoil@users.noreply.github.com>
2026-06-24 13:44:21 +0200

    chore: delete tmp files

--- tmp_stid_p9.py
+++
@@ -1,5 +0,0 @@
-import fitz
-d = fitz.open(r"task_solve/2026-06-22_jsv_inlet_separator_diameter/step1_scope_and_research/references/07316655.PDF")
-t = d[8].get_text()
-print("LEN", len(t))
-print(t)
