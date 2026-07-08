b058e715e40ea0df8d8edd022c68b25ade06f802
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-07-08 23:30:23 +0200

    update

--- src/test/java/neqsim/DocExamplesCompilationTest.java
+++ src/test/java/neqsim/DocExamplesCompilationTest.java
@@ -576,13 +576,11 @@ public class DocExamplesCompilationTest {
     column.getReboiler().setOutTemperature(105.0 + 273.15);
     column.setTopPressure(30.0);
     column.setBottomPressure(32.0);
-    column.setMaxNumberOfIterations(400);
     column.setSolverType(DistillationColumn.SolverType.MESH_RESIDUAL);
     column.run();
 
-    System.out.println("EXP doc MESH iterations=" + column.getLastIterationCount() + " solved=" + column.solved()
-        + " tempRes=" + column.getLastTemperatureResidual());
     assertTrue(column.solved());
+    assertNotNull(column.getGasOutStream());
     assertNotNull(column.getLiquidOutStream());
     assertTrue(column.getTrays().size() > 0);
     assertTrue(Double.isFinite(column.getTray(0).getTemperature()));
