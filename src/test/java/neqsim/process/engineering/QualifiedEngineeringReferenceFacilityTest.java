6b3a8fb8f063c468000f21c39a67057c027c2204
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-07-17 20:38:43 +0200

    Expose dynamic safe-state diagnostics

--- src/test/java/neqsim/process/engineering/QualifiedEngineeringReferenceFacilityTest.java
+++ src/test/java/neqsim/process/engineering/QualifiedEngineeringReferenceFacilityTest.java
@@ -57,8 +57,10 @@ class QualifiedEngineeringReferenceFacilityTest {
         convergenceDiagnostics(result.getSimulation().getEngineeringDesignLoopResult()));
     assertFalse(result.getSimulation().getCaseRunReport().getEnvelope().hasCaseFailures());
     assertTrue(result.getQualification().isQualifiedForControlledPilot(),
-        result.getQualification().getFailedGates().toString());
-    assertTrue(result.getSimulation().getDynamicScenarioResults().get(0).isPassed());
+        result.getQualification().toMap() + " dynamic="
+            + result.getSimulation().getDynamicScenarioResults().get(0).toMap());
+    assertTrue(result.getSimulation().getDynamicScenarioResults().get(0).isPassed(),
+        result.getSimulation().getDynamicScenarioResults().get(0).toMap().toString());
     assertTrue(result.getSimulation().getCoupledSafetyResults().get(0).getValue().isCapacityAcceptable());
     assertTrue(Files.isRegularFile(result.getCompilation().getDexpiResult().getDexpi20File()));
     assertTrue(Files.isRegularFile(result.getCompilation().getCompilerManifestFile()));
