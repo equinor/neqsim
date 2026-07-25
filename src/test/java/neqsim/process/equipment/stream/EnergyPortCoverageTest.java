31613fc2b8a145278f10cea747746dbdc1e4bf2b
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-07-25 18:24:14 +0200

    style: format final energy port rollout

--- src/test/java/neqsim/process/equipment/stream/EnergyPortCoverageTest.java
+++ src/test/java/neqsim/process/equipment/stream/EnergyPortCoverageTest.java
@@ -30,147 +30,60 @@ class EnergyPortCoverageTest {
 
   @Test
   void testRotatingEquipmentPorts() {
-    assertPort(
-        new Pump("pump"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.INPUT,
+    assertPort(new Pump("pump"), "shaftPower", EnergyType.SHAFT_WORK, EnergyPortDirection.INPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Compressor("compressor"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.INPUT,
+    assertPort(new Compressor("compressor"), "shaftPower", EnergyType.SHAFT_WORK, EnergyPortDirection.INPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Expander("expander"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.OUTPUT,
+    assertPort(new Expander("expander"), "shaftPower", EnergyType.SHAFT_WORK, EnergyPortDirection.OUTPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new SteamTurbine("steam turbine"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.OUTPUT,
+    assertPort(new SteamTurbine("steam turbine"), "shaftPower", EnergyType.SHAFT_WORK, EnergyPortDirection.OUTPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new GasTurbine("gas turbine"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.OUTPUT,
+    assertPort(new GasTurbine("gas turbine"), "shaftPower", EnergyType.SHAFT_WORK, EnergyPortDirection.OUTPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new GasTurbineUnit("catalogue gas turbine"),
-        "shaftPower",
-        EnergyType.SHAFT_WORK,
-        EnergyPortDirection.OUTPUT,
-        EnergyPortMode.CALCULATED);
+    assertPort(new GasTurbineUnit("catalogue gas turbine"), "shaftPower", EnergyType.SHAFT_WORK,
+        EnergyPortDirection.OUTPUT, EnergyPortMode.CALCULATED);
   }
 
   @Test
   void testThermalEquipmentPorts() {
-    assertPort(
-        new Heater("heater"),
-        "heatDuty",
-        EnergyType.HEAT,
-        EnergyPortDirection.BIDIRECTIONAL,
+    assertPort(new Heater("heater"), "heatDuty", EnergyType.HEAT, EnergyPortDirection.BIDIRECTIONAL,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Cooler("cooler"),
-        "heatDuty",
-        EnergyType.HEAT,
-        EnergyPortDirection.BIDIRECTIONAL,
+    assertPort(new Cooler("cooler"), "heatDuty", EnergyType.HEAT, EnergyPortDirection.BIDIRECTIONAL,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Condenser("condenser"),
-        "heatDuty",
-        EnergyType.HEAT,
-        EnergyPortDirection.OUTPUT,
+    assertPort(new Condenser("condenser"), "heatDuty", EnergyType.HEAT, EnergyPortDirection.OUTPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Reboiler("reboiler"),
-        "heatDuty",
-        EnergyType.HEAT,
-        EnergyPortDirection.INPUT,
+    assertPort(new Reboiler("reboiler"), "heatDuty", EnergyType.HEAT, EnergyPortDirection.INPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new AmmoniaSynthesisReactor("ammonia reactor"),
-        "reactionHeat",
-        EnergyType.HEAT,
-        EnergyPortDirection.OUTPUT,
-        EnergyPortMode.CALCULATED);
+    assertPort(new AmmoniaSynthesisReactor("ammonia reactor"), "reactionHeat", EnergyType.HEAT,
+        EnergyPortDirection.OUTPUT, EnergyPortMode.CALCULATED);
     StirredTankReactor reactor = new StirredTankReactor("CSTR");
-    assertPort(
-        reactor,
-        "heatDuty",
-        EnergyType.HEAT,
-        EnergyPortDirection.BIDIRECTIONAL,
-        EnergyPortMode.CALCULATED);
-    assertPort(
-        reactor,
-        "agitatorPower",
-        EnergyType.ELECTRICAL,
-        EnergyPortDirection.INPUT,
-        EnergyPortMode.CALCULATED);
-    assertPort(
-        new GasTurbine("gas turbine"),
-        "exhaustHeat",
-        EnergyType.HEAT,
-        EnergyPortDirection.OUTPUT,
+    assertPort(reactor, "heatDuty", EnergyType.HEAT, EnergyPortDirection.BIDIRECTIONAL, EnergyPortMode.CALCULATED);
+    assertPort(reactor, "agitatorPower", EnergyType.ELECTRICAL, EnergyPortDirection.INPUT, EnergyPortMode.CALCULATED);
+    assertPort(new GasTurbine("gas turbine"), "exhaustHeat", EnergyType.HEAT, EnergyPortDirection.OUTPUT,
         EnergyPortMode.CALCULATED);
   }
 
   @Test
   void testElectricalEquipmentPorts() {
-    ProcessEquipmentBaseClass[] generators = {
-      new SolarPanel("solar"),
-      new WindTurbine("wind turbine"),
-      new WindFarm("wind farm"),
-      new FuelCell("fuel cell"),
-      new CombinedCycleSystem("combined cycle")
-    };
+    ProcessEquipmentBaseClass[] generators = { new SolarPanel("solar"), new WindTurbine("wind turbine"),
+        new WindFarm("wind farm"), new FuelCell("fuel cell"), new CombinedCycleSystem("combined cycle") };
     for (ProcessEquipmentBaseClass generator : generators) {
-      assertPort(
-          generator,
-          "electricalPower",
-          EnergyType.ELECTRICAL,
-          EnergyPortDirection.OUTPUT,
+      assertPort(generator, "electricalPower", EnergyType.ELECTRICAL, EnergyPortDirection.OUTPUT,
           EnergyPortMode.CALCULATED);
     }
 
-    assertPort(
-        new BatteryStorage("battery"),
-        "electricalPower",
-        EnergyType.ELECTRICAL,
-        EnergyPortDirection.BIDIRECTIONAL,
+    assertPort(new BatteryStorage("battery"), "electricalPower", EnergyType.ELECTRICAL,
+        EnergyPortDirection.BIDIRECTIONAL, EnergyPortMode.CALCULATED);
+    assertPort(new Electrolyzer("electrolyzer"), "electricalPower", EnergyType.ELECTRICAL, EnergyPortDirection.INPUT,
         EnergyPortMode.CALCULATED);
-    assertPort(
-        new Electrolyzer("electrolyzer"),
-        "electricalPower",
-        EnergyType.ELECTRICAL,
-        EnergyPortDirection.INPUT,
-        EnergyPortMode.CALCULATED);
-    assertPort(
-        new CO2Electrolyzer("CO2 electrolyzer"),
-        "electricalPower",
-        EnergyType.ELECTRICAL,
-        EnergyPortDirection.INPUT,
-        EnergyPortMode.CALCULATED);
-    assertPort(
-        new BioFeedstockPreparation("feed preparation"),
-        "electricalPower",
-        EnergyType.ELECTRICAL,
-        EnergyPortDirection.INPUT,
-        EnergyPortMode.CALCULATED);
+    assertPort(new CO2Electrolyzer("CO2 electrolyzer"), "electricalPower", EnergyType.ELECTRICAL,
+        EnergyPortDirection.INPUT, EnergyPortMode.CALCULATED);
+    assertPort(new BioFeedstockPreparation("feed preparation"), "electricalPower", EnergyType.ELECTRICAL,
+        EnergyPortDirection.INPUT, EnergyPortMode.CALCULATED);
   }
 
-  private static void assertPort(
-      ProcessEquipmentBaseClass equipment,
-      String portName,
-      EnergyType type,
-      EnergyPortDirection direction,
-      EnergyPortMode mode) {
+  private static void assertPort(ProcessEquipmentBaseClass equipment, String portName, EnergyType type,
+      EnergyPortDirection direction, EnergyPortMode mode) {
     EnergyPort port = equipment.getEnergyPort(portName);
     assertNotNull(port);
     assertEquals(type, port.getEnergyType());
