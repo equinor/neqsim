0a78742e40d45407983b50f4a25beb9df51ade9d
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-07-25 23:47:18 +0200

    feat: report chemical fuel energy

--- src/main/java/neqsim/process/equipment/stream/EnergyNetworkReport.java
+++ src/main/java/neqsim/process/equipment/stream/EnergyNetworkReport.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import com.google.gson.GsonBuilder;
+import neqsim.util.unit.PowerUnit;
 
 /**
  * Auditable result of an {@link EnergyBus} balance calculation.
@@ -26,6 +27,7 @@ public final class EnergyNetworkReport implements Serializable {
   private final double balancingGeneration;
   private final double balancingConsumption;
   private final double conversionLoss;
+  private final double fuelEnergyRate;
   private final double operatingCostPerHour;
   private final double co2EmissionRate;
 
@@ -43,13 +45,14 @@ public final class EnergyNetworkReport implements Serializable {
    * @param balancingGeneration balancing generation in W
    * @param balancingConsumption balancing consumption in W
    * @param conversionLoss conversion loss in W
+   * @param fuelEnergyRate accepted chemical or fuel energy rate in W
    * @param operatingCostPerHour operating cost per hour
    * @param co2EmissionRate CO2-equivalent emission rate in kg/h
    */
   public EnergyNetworkReport(String busName, List<EnergyAllocation> allocations, double offeredSupply,
       double acceptedSupply, double requestedDemand, double servedDemand, double unmetDemand, double curtailedSupply,
-      double balancingGeneration, double balancingConsumption, double conversionLoss, double operatingCostPerHour,
-      double co2EmissionRate) {
+      double balancingGeneration, double balancingConsumption, double conversionLoss, double fuelEnergyRate,
+      double operatingCostPerHour, double co2EmissionRate) {
     this.busName = busName;
     this.allocations = new ArrayList<EnergyAllocation>(allocations);
     this.offeredSupply = offeredSupply;
@@ -61,6 +64,7 @@ public final class EnergyNetworkReport implements Serializable {
     this.balancingGeneration = balancingGeneration;
     this.balancingConsumption = balancingConsumption;
     this.conversionLoss = conversionLoss;
+    this.fuelEnergyRate = fuelEnergyRate;
     this.operatingCostPerHour = operatingCostPerHour;
     this.co2EmissionRate = co2EmissionRate;
   }
@@ -162,6 +166,25 @@ public final class EnergyNetworkReport implements Serializable {
    */
   public double getConversionLoss() {
     return conversionLoss;
+  }
+
+  /**
+   * Gets accepted chemical or fuel energy rate.
+   *
+   * @return fuel energy rate in W
+   */
+  public double getFuelEnergyRate() {
+    return fuelEnergyRate;
+  }
+
+  /**
+   * Gets accepted chemical or fuel energy rate in a requested power unit.
+   *
+   * @param unit requested power unit
+   * @return fuel energy rate in the requested unit
+   */
+  public double getFuelEnergyRate(String unit) {
+    return new PowerUnit(fuelEnergyRate, "W").getValue(unit);
   }
 
   /**
