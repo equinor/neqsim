2ab33f08ec97688ccaab182c55da5fcb35d41b77
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-07-31 01:43:59 +0200

    fix: wrap three-fluid holdup regression data

--- src/test/java/neqsim/process/equipment/pipeline/twophasepipe/ThreeFluidSectionTest.java
+++ src/test/java/neqsim/process/equipment/pipeline/twophasepipe/ThreeFluidSectionTest.java
@@ -136,7 +136,9 @@ class ThreeFluidSectionTest {
   @Test
   void testThreeLayerGeometryConservesSegmentAreaAcrossPipeDiameters() {
     double[] diameters = { 0.05, 0.10, 0.30, 1.0, 2.0 };
-    double[][] holdupSets = { { 0.94, 0.05, 0.01 }, { 0.80, 0.15, 0.05 }, { 0.50, 0.30, 0.20 }, { 0.10, 0.45, 0.45 } };
+    double[][] holdupSets = {
+        { 0.94, 0.05, 0.01 }, { 0.80, 0.15, 0.05 }, { 0.50, 0.30, 0.20 },
+        { 0.10, 0.45, 0.45 } };
 
     for (double diameter : diameters) {
       for (double[] holdups : holdupSets) {
