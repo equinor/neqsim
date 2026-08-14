ef5abef9a934ec8eab68a6a1222a6a0e5b06b805
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-08-14 19:10:27 +0200

    fix(io): explicitly manage water OLGA output stream

--- src/main/java/neqsim/thermodynamicoperations/propertygenerator/OLGApropertyTableGeneratorWaterEven.java
+++ src/main/java/neqsim/thermodynamicoperations/propertygenerator/OLGApropertyTableGeneratorWaterEven.java
@@ -718,7 +718,8 @@ public class OLGApropertyTableGeneratorWaterEven extends neqsim.thermodynamicope
      * thermoSystem.setTemperature(temperatures[j]); writer.write(ROG[i][j] + ","); } } writer.write(")"); } catch
      * (IOException ex) { // report } finally { try { } writer.close(); } catch (Exception ex) { } }
      */
-    try (Writer writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(filename), "utf-8"))) {
+    try (FileOutputStream outputStream = new FileOutputStream(filename);
+        Writer writer = new BufferedWriter(new OutputStreamWriter(outputStream, "utf-8"))) {
       writer.write("'WATER-OPTION ENTROPY NONEQ '" + "\n");
       writer.write(pressures.length + "   " + temperatures.length + "    " + RSWTOB + "\n");
       int Pcounter = 0;
