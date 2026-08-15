1c2666544fb62469c28f9d93d771a5353ee0b8e7
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-08-15 13:33:35 +0200

    Fix numeric precision test line breaks

--- src/test/java/neqsim/process/processmodel/dexpi/DexpiXmlWriterTest.java
+++ src/test/java/neqsim/process/processmodel/dexpi/DexpiXmlWriterTest.java
@@ -342,7 +342,9 @@ public class DexpiXmlWriterTest extends NeqSimTest {
     String secondXml = secondOut.toString(StandardCharsets.UTF_8.name());
 
     assertTrue(firstXml.contains("Name=\"OperatingTemperatureValue\" Unit=\"C\" Value=\"30.707918389\""));
-    assertEquals(normalizeEmissionMetadata(firstXml), normalizeEmissionMetadata(secondXml));\n    assertEquals("0.00000012345678901",\n        DexpiXmlWriter.formatNumericAttribute(0.0000001234567890123));
+    assertEquals(normalizeEmissionMetadata(firstXml), normalizeEmissionMetadata(secondXml));
+    assertEquals("0.00000012345678901",
+        DexpiXmlWriter.formatNumericAttribute(0.0000001234567890123));
   }
 
   private static String normalizeEmissionMetadata(String xml) {
