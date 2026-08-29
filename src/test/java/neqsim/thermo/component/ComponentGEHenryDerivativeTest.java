548b2e7ed65023b2062601f40968bd9d9b7bc531
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-08-29 11:08:46 +0200

    style: apply Spotless to Henry derivative tests

--- src/test/java/neqsim/thermo/component/ComponentGEHenryDerivativeTest.java
+++ src/test/java/neqsim/thermo/component/ComponentGEHenryDerivativeTest.java
@@ -12,29 +12,31 @@ class ComponentGEHenryDerivativeTest {
   @Test
   void activeCorrelationReturnsLogarithmicDerivative() {
     ComponentGeNRTL component = new ComponentGeNRTL("CO2", 1.0, 1.0, 0);
-    component.setHenryCoefParameter(new double[] {10.0, -1200.0, 1.5, 0.002});
+    component.setHenryCoefParameter(new double[] { 10.0, -1200.0, 1.5, 0.002 });
     PhasePitzer phase = phaseAt(TEMPERATURE);
 
     double expected = finiteDifferenceLogHenry(component, TEMPERATURE);
 
     assertEquals(expected, component.fugcoefDiffTemp(phase), 1.0e-9);
-    assertEquals(1200.0 / (TEMPERATURE * TEMPERATURE) + 1.5 / TEMPERATURE + 0.002,
+    assertEquals(
+        1200.0 / (TEMPERATURE * TEMPERATURE) + 1.5 / TEMPERATURE + 0.002,
         component.fugcoefDiffTemp(phase), 1.0e-12);
   }
 
   @Test
   void pitzerNeutralGasUsesSameLogarithmicDerivative() {
     ComponentGePitzer component = new ComponentGePitzer("CO2", 1.0, 1.0, 0);
-    component.setHenryCoefParameter(new double[] {8.0, -800.0, 0.5, 0.001});
+    component.setHenryCoefParameter(new double[] { 8.0, -800.0, 0.5, 0.001 });
     PhasePitzer phase = phaseAt(TEMPERATURE);
 
-    assertEquals(finiteDifferenceLogHenry(component, TEMPERATURE), component.fugcoefDiffTemp(phase), 1.0e-9);
+    assertEquals(
+        finiteDifferenceLogHenry(component, TEMPERATURE), component.fugcoefDiffTemp(phase), 1.0e-9);
   }
 
   @Test
   void failClosedCorrelationHasZeroReferenceDerivative() {
     ComponentGeNRTL component = new ComponentGeNRTL("CO2", 1.0, 1.0, 0);
-    component.setHenryCoefParameter(new double[] {1000.0, 0.0, 0.0, 0.0});
+    component.setHenryCoefParameter(new double[] { 1000.0, 0.0, 0.0, 0.0 });
 
     assertTrue(Double.isInfinite(component.getHenryCoef(TEMPERATURE)));
     assertEquals(0.0, component.fugcoefDiffTemp(phaseAt(TEMPERATURE)), 0.0);
@@ -43,7 +45,7 @@ class ComponentGEHenryDerivativeTest {
   @Test
   void unsupportedPitzerHydrocarbonHasZeroReferenceDerivative() {
     ComponentGePitzer component = new ComponentGePitzer("methane", 1.0, 1.0, 0);
-    component.setHenryCoefParameter(new double[] {0.0, 0.0, 0.0, 0.01});
+    component.setHenryCoefParameter(new double[] { 0.0, 0.0, 0.0, 0.01 });
 
     double rawLogDerivative = component.getHenryCoefdT(TEMPERATURE) / component.getHenryCoef(TEMPERATURE);
 
