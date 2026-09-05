aabeceb5aef627d05100248c2b8f593de1d4dcef
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-09-05 19:56:48 +0200

    style(twofluid): format rejection assertion

--- src/test/java/neqsim/process/equipment/pipeline/twophasepipe/CoupledPressureMomentumTengesdalProgressTest.java
+++ src/test/java/neqsim/process/equipment/pipeline/twophasepipe/CoupledPressureMomentumTengesdalProgressTest.java
@@ -142,9 +142,9 @@ class CoupledPressureMomentumTengesdalProgressTest {
 
     assertEquals(5.0, pipe.getSimulationTime(), 1.0e-9);
     assertFalse(pipe.isTransientOutletBackflowClamped());
-    assertFalse(pipe.isTransientCoupledPressureMomentumFailureDetected(), "refined coupled solve recorded "
-        + pipe.getTransientCoupledPressureMomentumRejectedSubsteps() + " rejected substeps: "
-        + pipe.getTransientCoupledPressureMomentumFailureDiagnostic());
+    assertFalse(pipe.isTransientCoupledPressureMomentumFailureDetected(),
+        "refined coupled solve recorded " + pipe.getTransientCoupledPressureMomentumRejectedSubsteps()
+            + " rejected substeps: " + pipe.getTransientCoupledPressureMomentumFailureDiagnostic());
     assertEquals(0, pipe.getTransientCoupledPressureMomentumRejectedSubsteps());
   }
 
