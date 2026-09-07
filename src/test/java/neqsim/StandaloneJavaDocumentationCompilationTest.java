8fef459de755d8435e11e4dd8bbe410152f490d0
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-09-07 14:04:12 +0200

    test(docs): cover two-fluid pipe logging policy

--- src/test/java/neqsim/StandaloneJavaDocumentationCompilationTest.java
+++ src/test/java/neqsim/StandaloneJavaDocumentationCompilationTest.java
@@ -35,7 +35,8 @@ import org.junit.jupiter.api.io.TempDir;
 public class StandaloneJavaDocumentationCompilationTest {
   private static final int EXPECTED_EXAMPLE_COUNT = 14;
   private static final List<String> LOG4J2_EXAMPLES = Arrays.asList("EclipseE300ExportImportExample.java",
-      "FlowRegimeDebug.java", "FlowRegimeDetectionExample.java", "RealTimeIntegrationExample.java");
+      "FlowRegimeDebug.java", "FlowRegimeDetectionExample.java", "RealTimeIntegrationExample.java",
+      "TwoFluidPipeExample.java");
 
   @TempDir
   Path compilationOutput;
