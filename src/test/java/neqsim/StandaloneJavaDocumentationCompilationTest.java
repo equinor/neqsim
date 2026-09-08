1df599a4fe52dda73ae11c9a86fd1eff8f5041b5
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-09-08 14:17:12 +0200

    style(examples): apply hosted Spotless formatting

--- src/test/java/neqsim/StandaloneJavaDocumentationCompilationTest.java
+++ src/test/java/neqsim/StandaloneJavaDocumentationCompilationTest.java
@@ -37,8 +37,7 @@ public class StandaloneJavaDocumentationCompilationTest {
   private static final List<String> LOG4J2_EXAMPLES = Arrays.asList("EclipseE300ExportImportExample.java",
       "FlowRegimeDebug.java", "FlowRegimeDetectionExample.java", "MultiScenarioVFPExample.java",
       "MultiphaseModelPressureDropComparison.java", "OffshoreEmissionReportingExample.java",
-      "RealTimeIntegrationExample.java", "SlugTrackingComparisonExample.java",
-      "TwoFluidPipeExample.java");
+      "RealTimeIntegrationExample.java", "SlugTrackingComparisonExample.java", "TwoFluidPipeExample.java");
 
   @TempDir
   Path compilationOutput;
