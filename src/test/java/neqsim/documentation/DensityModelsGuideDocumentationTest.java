dd2bd59c1a87da304a0a3c996c266a374bbe89c7
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-09-17 14:11:13 +0200

    test: close density model marker literals

--- src/test/java/neqsim/documentation/DensityModelsGuideDocumentationTest.java
+++ src/test/java/neqsim/documentation/DensityModelsGuideDocumentationTest.java
@@ -53,10 +53,10 @@ public class DensityModelsGuideDocumentationTest extends neqsim.NeqSimTest {
     assertTrue(costaldTest.contains("setLiquidDensityModel(\"COSTALD\")"));
 
     for (String required : Arrays.asList(
-        "\"Peneloux\",
-        "\"COSTALD\",
-        "\"NASTALD\",
-        "\"Rackett\",
+        "\"Peneloux\"",
+        "\"COSTALD\"",
+        "\"NASTALD\"",
+        "\"Rackett\"",
         "Do not tune `setVolumeCorrectionConst(...)` from a single density point",
         "validate against independent data")) {
       assertTrue(guide.contains(required), "Missing maintained density guidance: " + required);
