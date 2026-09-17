22ce771ba740b01fc711ff0c8722465834afe35b
Even Solbraa <41290109+EvenSol@users.noreply.github.com>
2026-09-17 13:06:19 +0200

    style: apply hosted Spotless formatting

--- src/main/java/neqsim/process/equipment/reactor/AqueousHydrogenSulfideOxidationS8TransferLedger.java
+++ src/main/java/neqsim/process/equipment/reactor/AqueousHydrogenSulfideOxidationS8TransferLedger.java
@@ -203,10 +203,9 @@ public final class AqueousHydrogenSulfideOxidationS8TransferLedger {
         double totalUnallocatedSulfurEquivalentMassKg, double massClosureResidualKg) {
       this.ledgerIdentifier = ledgerIdentifier;
       this.productIdentityBasisIdentifier = productIdentityBasisIdentifier;
-      this.batches = Collections.unmodifiableList(
-          new ArrayList<AqueousHydrogenSulfideOxidationS8TransferBatch.Result>(batches));
-      this.downstreamIdempotencyKeys =
-          Collections.unmodifiableSet(new HashSet<String>(downstreamIdempotencyKeys));
+      this.batches = Collections
+          .unmodifiableList(new ArrayList<AqueousHydrogenSulfideOxidationS8TransferBatch.Result>(batches));
+      this.downstreamIdempotencyKeys = Collections.unmodifiableSet(new HashSet<String>(downstreamIdempotencyKeys));
       this.transferCount = transferCount;
       this.totalSourceSulfurEquivalentMassKg = totalSourceSulfurEquivalentMassKg;
       this.totalTransferredS8MassKg = totalTransferredS8MassKg;
